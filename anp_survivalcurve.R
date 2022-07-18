a8ae678d330beead7c52e54b333b96a8497089a1
HelenMylne <hkm513@york.ac.uk>
2022-07-18 19:38:31 +0100

    only consider males, fix bug in basta data set up

--- anp_survivalcurve.R
+++ anp_survivalcurve.R
@@ -104,42 +104,20 @@ stopCluster(cl)
 
 #### Try with Amboseli data ####
 ## read in Amboseli data ####
-all_nodes <- readxl::read_excel('data_raw/Raw_ATE_AllElephants_Lee220118.xlsx') %>% 
-  select(ID, Sex,
-         `Birth_month(Jan<1971)`, Birth_year, Birth_accuracy,
-         Death_month, Death_Year, Death_accuracy, Age_death) %>% 
+males <- readxl::read_excel('data_raw/Raw_ATE_Males_Lee220121.xlsx') %>% 
+  select(CASENAME, BYR, DYR) %>% 
   janitor::clean_names()
-str(all_nodes)
-boxplot(as.numeric(all_nodes$age_death) ~ all_nodes$death_accuracy) # adds NA values for anything classed as "living"
-
-# create dataframe that only considers node ages
-all_nodes$id_no <- ifelse(all_nodes$sex == 'unknown', paste('U', all_nodes$id, sep = ''),
-                          ifelse(all_nodes$sex == 'Female', paste('F', all_nodes$id, sep = ''),
-                                 ifelse(all_nodes$sex == 'Male', paste('M', all_nodes$id, sep = ''),
-                                        'check')))
-which(all_nodes$id_no == 'check')
+str(males)
 
 # generate numeric variable for age in 2021 of living elephants or age at death
-all_nodes$age_death_num <- as.numeric(ifelse(all_nodes$age_death == 'living',
-                                             2021 - all_nodes$birth_year,
-                                             all_nodes$age_death))
-all_nodes$age_death_round <- round(all_nodes$age_death_num, 0)
-table(all_nodes$age_death_round, all_nodes$sex)
-hist(all_nodes$age_death_round, breaks = 75)                                    # overall age distribution
-hist(all_nodes$age_death_round[which(all_nodes$sex == 'Male')], breaks = 70)    # male age distribution
-hist(all_nodes$age_death_round[which(all_nodes$sex == 'Female')], breaks = 75)  # female age distribution
-all_nodes <- all_nodes[,c(10,1,2,4,7,9,11,12)]
+males$age <- as.numeric(ifelse(males$dyr < 10, 2021 - males$byr, males$dyr - males$byr))
+table(males$age)
+hist(males$age, breaks = 75)    # overall age distribution
 
 # create new variable
-all_nodes$censor <- ifelse(all_nodes$age_death == 'living', 'TRUE', 'FALSE')    # censor = TRUE when elephant is still alive
-table(all_nodes$censor)
-
-# clean up
-basta_data <- all_nodes[!is.na(all_nodes$age_death_num),
-                        c('id_no','id','birth_year','death_year',
-                          'age_death_num','censor','sex')] # single data frame for age at death/2021
-nrow(basta_data) - length(unique(basta_data$id))           # 0 -- every elephant has a unique number
-rm(all_nodes)
+males$censor <- ifelse(males$dyr < 10, 'TRUE', 'FALSE')    # censor = TRUE when elephant is still alive
+table(males$censor)
+nrow(males) - length(unique(males$casename))               # 0 -- every elephant has a unique number
 
 ## create data frame for basta ####
 ?basta # object = A data.frame to be used as an input data file for BaSTA. The first column is a vector of individual unique IDs, the second and third columns are birth and death years respectively. Columns 4, …, n_t-1 represent the observation window of n_t years. This is followed (optionally) by columns for categorical and continuous covariates (see details).
@@ -156,19 +134,19 @@ plot(Out, trace.name = "gamma") # Plot traces for proportional hazards parameter
 plot(Out, plot.trace = FALSE)   # Plot survival and mortality curves
 
 # Create columns for sightings in each year
-basta_data$obs_1972 <- NA ; basta_data$obs_1973 <- NA ; basta_data$obs_1974 <- NA ; basta_data$obs_1975 <- NA
-basta_data$obs_1976 <- NA ; basta_data$obs_1977 <- NA ; basta_data$obs_1978 <- NA ; basta_data$obs_1979 <- NA
-basta_data$obs_1980 <- NA ; basta_data$obs_1981 <- NA ; basta_data$obs_1982 <- NA ; basta_data$obs_1983 <- NA
-basta_data$obs_1984 <- NA ; basta_data$obs_1985 <- NA ; basta_data$obs_1986 <- NA ; basta_data$obs_1987 <- NA
-basta_data$obs_1988 <- NA ; basta_data$obs_1989 <- NA ; basta_data$obs_1990 <- NA ; basta_data$obs_1991 <- NA
-basta_data$obs_1992 <- NA ; basta_data$obs_1993 <- NA ; basta_data$obs_1994 <- NA ; basta_data$obs_1995 <- NA
-basta_data$obs_1996 <- NA ; basta_data$obs_1997 <- NA ; basta_data$obs_1998 <- NA ; basta_data$obs_1999 <- NA
-basta_data$obs_2000 <- NA ; basta_data$obs_2001 <- NA ; basta_data$obs_2002 <- NA ; basta_data$obs_2003 <- NA
-basta_data$obs_2004 <- NA ; basta_data$obs_2005 <- NA ; basta_data$obs_2006 <- NA ; basta_data$obs_2007 <- NA
-basta_data$obs_2008 <- NA ; basta_data$obs_2009 <- NA ; basta_data$obs_2010 <- NA ; basta_data$obs_2011 <- NA
-basta_data$obs_2012 <- NA ; basta_data$obs_2013 <- NA ; basta_data$obs_2014 <- NA ; basta_data$obs_2015 <- NA
-basta_data$obs_2016 <- NA ; basta_data$obs_2017 <- NA ; basta_data$obs_2018 <- NA ; basta_data$obs_2019 <- NA
-basta_data$obs_2020 <- NA ; basta_data$obs_2021 <- NA
+males$obs_1972 <- NA ; males$obs_1973 <- NA ; males$obs_1974 <- NA ; males$obs_1975 <- NA
+males$obs_1976 <- NA ; males$obs_1977 <- NA ; males$obs_1978 <- NA ; males$obs_1979 <- NA
+males$obs_1980 <- NA ; males$obs_1981 <- NA ; males$obs_1982 <- NA ; males$obs_1983 <- NA
+males$obs_1984 <- NA ; males$obs_1985 <- NA ; males$obs_1986 <- NA ; males$obs_1987 <- NA
+males$obs_1988 <- NA ; males$obs_1989 <- NA ; males$obs_1990 <- NA ; males$obs_1991 <- NA
+males$obs_1992 <- NA ; males$obs_1993 <- NA ; males$obs_1994 <- NA ; males$obs_1995 <- NA
+males$obs_1996 <- NA ; males$obs_1997 <- NA ; males$obs_1998 <- NA ; males$obs_1999 <- NA
+males$obs_2000 <- NA ; males$obs_2001 <- NA ; males$obs_2002 <- NA ; males$obs_2003 <- NA
+males$obs_2004 <- NA ; males$obs_2005 <- NA ; males$obs_2006 <- NA ; males$obs_2007 <- NA
+males$obs_2008 <- NA ; males$obs_2009 <- NA ; males$obs_2010 <- NA ; males$obs_2011 <- NA
+males$obs_2012 <- NA ; males$obs_2013 <- NA ; males$obs_2014 <- NA ; males$obs_2015 <- NA
+males$obs_2016 <- NA ; males$obs_2017 <- NA ; males$obs_2018 <- NA ; males$obs_2019 <- NA
+males$obs_2020 <- NA ; males$obs_2021 <- NA
 
 sightings <- readxl::read_excel('data_raw/Raw_ATE_Sightings_Fishlock220224.xlsx') %>% janitor::clean_names()
 sightings$year <- lubridate::year(sightings$obs_date)
@@ -177,15 +155,12 @@ colnames(sightings)[1] <- 'id'
 tapply(X = sightings$id, INDEX = sightings$year, FUN = table)
 
 (N <- 2022-1972)
-for(i in 1:nrow(basta_data)){
+for(i in 1:nrow(males)){
   for(j in 1:N){
-    casename <- sightings[sightings$id == basta_data$id[i],]
-    basta_data[i,j+7] <- ifelse(length(which(casename$year == j+1971)) > 0, 1, 0)
+    casename <- sightings[sightings$id == males$casename[i],]
+    males[i,j+5] <- ifelse(length(which(casename$year == j+1971)) > 0, 1, 0)
   }
 }
-basta_data$female <- ifelse(basta_data$sex == 'Female',1,0)
-basta_data$male <- ifelse(basta_data$sex == 'Male',1,0)
-basta_data$unknown <- ifelse(basta_data$sex == 'unknown',1,0)
 
 ### Preparing input data
 # Getting start and end times from the input datasets - will depend on your input if this works, but can also manually assign the year the observations began and ended
@@ -193,8 +168,8 @@ Ss <- 1972  # start of study
 Se <- 2021  # end of study
 
 # Set the arguments for the iterations
-iter <- 500000   # number of iterations
-sim <- 4         # number of chains
+iter <- 5000   # number of iterations
+sim <- 1         # number of chains
 warmup <- iter/2 # number of iterations to use during warmup/burn-in period
 thin <- 101      # thinning number
 
@@ -202,19 +177,31 @@ thin <- 101      # thinning number
 pM <-  c(-3,0.2,0.001,-4,0.05) # prior means for parameters in a Gompertz model with a bathtub shape
 pSD <- c(1,0.05,0.001,1,0.01)  # prior standard deviation for parameters in a Gompertz model with a bathtub shape
 
-InputData <- basta_data[,c(2:4,8:60)]
-Out <- basta(InputData, studyStart = Ss,
+basta_data <- males
+basta_data$dyr <- ifelse(basta_data$censor == 'TRUE', 0, basta_data$dyr)
+basta_data <- basta_data[-c(1:4,6,8,11,14:18,22,23,26:30,35,38,44:46,52,55,56,58,59,62,63,66,68,71,72,76,83,87,90,93,94,96,105,107,108,111,114,115,118,127,128,132,133,135,136,138,145,146,151,155,159,162,174,179,182,186,187,190,191,193,197,201,208,210,213,220,224,230,235,238,244,248,250,254,256,259,261,263,266,268,288,291,292,295,298,306,307,309,319,327,339,342,346,370,384,416,417,435,449,472,717,693,708,735,769,774),
+                         c(1:3,6:55)]
+Out <- basta(basta_data, studyStart = Ss,
              studyEnd = Se, nsim = sim, niter = iter, 
              burnin = warmup, thinning = thin, 
              thetaPriorMean = pM,thetaPriorSd = pSD,
              models = c("GO"), shape = c("bathtub"))
+summary(Out)
+plot(Out)
 
 
-### Assigning either male or female to individuals of unknown sex -- THIS DOESN'T WORK WHEN THE PROPORTION OF UNKNOWNS IS SO LOW -- COMES OUT WITH NEGATIVE NUMBERS OF MALES/FEMALES NEEDED
-# Probability of unknowns being male, with assumption: the probability of unknowns being male is 1-adult male ratio
-numUnknown <- sum(basta_data[ , ncol(basta_data)])
-numMales <- sum(basta_data[ , length(basta_data)-1])
-numFemales <- sum(basta_data[ , length(basta_data)-2])
+
+
+
+
+
+
+
+
+### Assigning either male or female to individuals of unknown sex -- THIS DOESN'T WORK WHEN THE ONLY SIGHTINGS DATA I HAVE ARE FOR MALES!
+numUnknown <- sum(males[ , ncol(males)])
+numMales <- sum(males[ , length(males)-1])
+numFemales <- sum(males[ , length(males)-2])
 howManyMoreFemalesThanMales <- numFemales - numMales
 moreMalesNeeded <- howManyMoreFemalesThanMales + (0.5 * (numUnknown - howManyMoreFemalesThanMales))
 moreFemalesNeeded <- numUnknown - moreMalesNeeded
@@ -258,7 +245,7 @@ ncl <- 4     # number of cpus used in cluster - check how many cores you have av
 cl <- makeCluster(ncl) # create the cluster element
 doParallel::registerDoParallel(cl)
 
-InputList <- MakeInput(nperm,basta_data,p_female) # create input from function above
+InputList <- MakeInput(nperm,males,p_female) # create input from function above
 table(InputList[[10]]$male) ; table(InputList[[10]]$female)
 
 
