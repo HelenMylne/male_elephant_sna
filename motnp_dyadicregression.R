192ad2e6942113f75eb91296ae02d730d324a10f
HelenMylne <hkm513@york.ac.uk>
2022-07-18 17:34:04 +0100

    initial attempt to follow through JHart96 dyadic regression method

---
+++ motnp_dyadicregression.R
@@ -0,0 +1,213 @@
+### Bayesian analysis of ALERT data
+### Set up ####
+# load packages
+library(tidyverse)
+library(dplyr)
+library(rstan)
+library(rethinking)
+library(igraph)
+library(dagitty)
+library(cmdstanr)
+
+# information
+R.Version()
+rstan::stan_version()
+
+# set stan path
+set_cmdstan_path('/Users/helen/.cmdstanr/cmdstan-2.28.2')
+
+# set seed
+set.seed(12345)
+
+################ Create simulated data set ################
+### Population
+# 120 individuals: 50 male (10 each of ages 3-7), 50 female (10 each of ages 3-7), 20 unknown (10 each of ages 1-2)
+# males no preference for any other individual -- edge weight 0.2
+# females in 8 cliques -- edge weight 0.8, otherwise 0.2
+# unknowns attached to a single female -- edge weight 0.95
+
+# assign females to family units
+cliques_F <- sample(1:8,50,replace=T)
+table(cliques_F)
+
+# create data frame of all individuals
+population <- data.frame(id = rep(NA,120),
+                         num = c(1:50,1:50,1:20),
+                         sex = c(rep('M',50), rep('F',50), rep('U',20)),
+                         age = c(rep(c(3:7), each = 10), rep(c(3:7), each = 10), rep(c(1:2), each = 10)),
+                         clique = c(rep(NA, 50), cliques_F, rep(NA, 20)))
+population$id <- paste(population$sex, population$num, sep = '')
+
+# randomly assign calves to females
+mothers <- data.frame(id = population$id[101:120],
+                      family = sample(population$id[61:100], replace = F, size = 20))
+mothers
+population <- left_join(x = population, y = mothers, by = 'id')
+
+# need a clique for U elephants and a family for F elephants
+females <- population[population$sex == 'F', c(1,5,6)]
+unknown <- population[population$sex == 'U', c(1,5,6)]
+females$mother <- females$id ; unknown$mother <- unknown$family   # create variable to join by
+families <- left_join(x = unknown, y = females, by = 'mother')    # join to give unknowns a clique
+unknown <- families[,c(1,4,6)]
+
+families <- left_join(x = females, y = unknown, by = 'mother')    # join to give mothers their calf
+females <- families[,c(1,5,2)]
+
+colnames(unknown) <- c('id','mother','clique')
+colnames(females) <- c('id','mother','clique')
+families <- rbind(females, unknown)                               # put mother and calf data into a single data frame
+
+# join to combine male and female/unknown data together
+population <- left_join(x = population, y = families, by = 'id')
+population <- population[,c(1,3,4,7,8)]
+colnames(population)[c(4,5)] <- c('family','clique')
+
+### Create dyadic data frame
+dyads <- data.frame(id_1 = rep(population$id, each = 120), id_2 = rep(population$id, 120))               # create new data frame of all dyads
+dyads$same <- ifelse(dyads$id_1 == dyads$id_2, 'yes', 'no') ; dyads <- dyads[dyads$same == 'no', c(1,2)] # remove self-dyads (e.g. M1-M1)
+dyads$node_1 <- as.integer(as.factor(dyads$id_1)) ; dyads$node_2 <- as.integer(as.factor(dyads$id_2))    # create factor of  <- 
+dyads$dyad <- ifelse(dyads$node_1 < dyads$node_2,                               # create variable of dyads
+                     paste(dyads$id_1, dyads$id_2, sep = '_'),
+                     paste(dyads$id_2, dyads$id_1, sep = '_'))
+
+dyads <- data.frame(dyad = unique(dyads$dyad))                                  # create new data frame of only unique dyads
+dyads <- separate(dyads, col = dyad, into = c('id_1','id_2'), remove = FALSE)   # nodes columns
+dyads$node_1 <- as.integer(as.factor(dyads$id_1))                               # nodes columns
+dyads$node_2 <- as.integer(as.factor(dyads$id_2))                               # nodes columns
+dyads$dyad_id <- as.integer(as.factor(dyads$dyad))                              # dyad index variable
+
+population$id_1 <- population$id ; population$id_2 <- population$id  # variable to join information on each individual
+dyads <- left_join(x = dyads, y = population, by = 'id_1')           # add information about node 1
+dyads <- dyads[,c(1:6,8:11)]
+colnames(dyads)[c(3,7:10)] <- c('id_2','sex_1','age_1','family_1','clique_1')
+
+dyads <- left_join(x = dyads, y = population, by = 'id_2')           # add information about node 2
+dyads <- dyads[,c(1:10,12:15)]
+colnames(dyads)[c(2,11:14)] <- c('id_1','sex_2','age_2','family_2','clique_2')
+
+dyads <- dyads[,c(1:7,11,8,12,9,13,10,14)]
+
+### Assign type of dyad
+dyads$pair_type <- ifelse(is.na(dyads$clique_1) | is.na(dyads$clique_2), 'no_group',
+                          ifelse(dyads$clique_1 == dyads$clique_2,
+                                 ifelse(is.na(dyads$family_1) | is.na(dyads$family_2), 'group',
+                                        ifelse(dyads$family_1 == dyads$id_2 | dyads$family_2 == dyads$id_1,
+                                               'family', 'group')),
+                                 'no_group'))
+
+### Assign edge weight to dyad pairs
+for(i in 1:nrow(dyads)){
+  if(dyads$pair_type[i] == 'family')  {dyads$edge[i] <- rethinking::rbeta2(1,0.95,50)   # mother-calf: edge weight around 0.95
+  } else 
+    if(dyads$pair_type[i] == 'group') {dyads$edge[i] <- rethinking::rbeta2(1,0.8,50)    # same family, not mother-calf: edge ~ 0.8
+    } else 
+      dyads$edge[i] <- rethinking::rbeta2(1,0.20,50)                                        # different family: edge weight around 0.2
+}
+summary(dyads$edge)
+
+boxplot(dyads$edge ~ dyads$pair_type)                                                   # check simulations are around what I expected
+points(y = dyads$edge, x = rep(2, length(which(dyads$pair_type == 'group'))),           # had a few problems with this one, but looks good now
+       col = col.alpha('black',0.2), pch = 19)
+
+### clean up environment
+rm(families, females, mothers, unknown, cliques_F)
+
+### Sample observations
+N <- 20
+dyads$event_count <- rbinom(nrow(dyads), N, prob = dyads$edge)
+
+### plot simulated observations against assigned edge weight to check model input is correlates to true value
+plot(event_count ~ edge, data = dyads,
+     las = 1, ylab = 'sightings together', xlab = 'assigned edge weight',
+     pch = 16, col = col.alpha(rangi2, 0.2))
+
+### add additional columns (dyadic sex, age and dem_class)
+head(dyads)
+# assign each node to age category (adult, pubescent, juvenile or calf) from age class
+dyads$age_cat_1 <- ifelse(dyads$age_1 == 1, 'C', ifelse(dyads$age_1 == 2, 'J', ifelse(dyads$age_1 < 5, 'P', 'A')))
+dyads$age_cat_2 <- ifelse(dyads$age_2 == 1, 'C', ifelse(dyads$age_2 == 2, 'J', ifelse(dyads$age_2 < 5, 'P', 'A')))
+
+# combine age categories into single variable for dyad
+dyads$age_dyad <- ifelse(dyads$age_cat_1 == 'A',
+                         ifelse(dyads$age_cat_2 == 'A', 'A_A',
+                                ifelse(dyads$age_cat_2 == 'P', 'A_P',
+                                       ifelse(dyads$age_cat_2 == 'J', 'A_J', 'A_C'))),
+                         ifelse(dyads$age_cat_1 == 'P',
+                                ifelse(dyads$age_cat_2 == 'A', 'A_P',
+                                       ifelse(dyads$age_cat_2 == 'P', 'P_P',
+                                              ifelse(dyads$age_cat_2 == 'J', 'P_J', 'P_C'))),
+                                ifelse(dyads$age_cat_1 == 'J',
+                                       ifelse(dyads$age_cat_2 == 'A', 'A_J',
+                                              ifelse(dyads$age_cat_2 == 'P', 'P_J',
+                                                     ifelse(dyads$age_cat_2 == 'J', 'J_J', 'J_C'))),
+                                       ifelse(dyads$age_cat_2 == 'A', 'A_C',
+                                              ifelse(dyads$age_cat_2 == 'P', 'P_C',
+                                                     ifelse(dyads$age_cat_2 == 'J', 'J_C', 'C_C'))))))
+unique(dyads$age_dyad) # "P_P" "A_P" "P_C" "P_J" "A_A" "A_C" "A_J" "C_C" "J_C" "J_J"
+dyads$age_diff <- abs(dyads$age_1 - dyads$age_2)
+
+# create composite measure of demography per node (e.g. AM = adult male, CU = calf of unknown sex)
+dyads$dem_class_1 <- paste(dyads$age_cat_1, dyads$sex_1, sep = '')
+dyads$dem_class_2 <- paste(dyads$age_cat_2, dyads$sex_2, sep = '')
+
+# combine demographies into single variable for dyad
+dyads$demf_1 <- as.integer(as.factor(dyads$dem_class_1)) ; dyads$demf_2 <- as.integer(as.factor(dyads$dem_class_2))
+dyads$dem_dyad <- ifelse(dyads$demf_1 < dyads$demf_2,
+                         paste(dyads$dem_class_1, dyads$dem_class_2, sep = '_'),
+                         paste(dyads$dem_class_2, dyads$dem_class_1, sep = '_'))
+dyads$dem_dyad <- ifelse(dyads$dem_dyad == 'CU_JU', 'JU_CU',           # standardise dem_dyad so always in same order
+                         ifelse(dyads$dem_dyad == 'CU_PF', 'PF_CU',
+                                ifelse(dyads$dem_dyad == 'CU_PM','PM_CU',
+                                       ifelse(dyads$dem_dyad == 'JU_PF','PF_JU',
+                                              ifelse(dyads$dem_dyad == 'JU_PM','PM_JU',dyads$dem_dyad)))))
+dyads$dem_dyad_f <- as.integer(as.factor(dyads$dem_dyad))              # make index variable
+
+dyads$dem_diff <- ifelse(dyads$dem_class_1 == dyads$dem_class_2, 0, 1) # binary: same or different
+dyads$sex_dyad <- paste(dyads$sex_1, dyads$sex_2, sep = '_')           # composite sex variable for dyad
+dyads$sex_diff <- ifelse(dyads$sex_1 == dyads$sex_2, 0, 1)             # binary: same or different
+dyads <- dyads[,c(1:23,26:30)]                                         # remove demf variables -- not needed
+
+# together vs apart per dyad
+summary(dyads$event_count)             # together, all out of 20
+dyads$apart <- N - dyads$event_count   # apart = 20-together
+
+# clear environment
+rm(population, i, N)
+
+################ Dyadic regression -- ulam() ################
+### create data list -- can contain no NA values in any column, even if column is not specified in model
+simdat_ls <- list(
+  n_dyads  = nrow(dyads),          # total number of times one or other of the dyad was observed
+  together = dyads$event_count +1, # count number of sightings seen together -- can't work out how in ulam() to add the 1 because it's got a probelm with adding int+int[] again, so for the sake of testing have just adding it first
+  apart    = dyads$apart +1,       # count number of sightings seen apart -- can't work out how in ulam() to add the 1 because it's got a probelm with adding int+int[] again, so for the sake of testing have just adding it first
+  age_diff = dyads$age_diff)       # age difference between individuals
+
+test <- ulam(alist(
+  events ~ dbeta(together, apart),
+  weight ~ dnorm(mu, sigma),
+  mu <- events + b1 * age_diff, # + u_i[id_1] + u_j[id_2], -- should include individual effects but no idea how to do this so that unaffected by which is id_1 and which is id_2. Also I strongly suspect this is the wrong formula, but I couldn't see how weight and events linked together any other way than if the overall "SRI" became the intercept the age effect?
+  b1 ~ dnorm(-0.5,2),
+  sigma ~ dexp(1)),
+  data = simdat_ls, chains = 1, cores = 4)
+stancode(test)
+precis(test)
+traceplot(test)
+
+d_reg_test <- cmdstan_model("models/dyadic_regression_hkm_22.03.03.stan")
+d_reg_test
+
+### create data list -- can contain no NA values in any column, even if column is not specified in model
+simdat_ls <- list(
+  n_dyads  = nrow(dyads),       # total number of times one or other of the dyad was observed
+  together = dyads$event_count, # count number of sightings seen together
+  apart    = dyads$apart,       # count number of sightings seen apart
+  age_diff = dyads$age_diff)    # age difference between individuals
+
+### Fit model
+test2 <- d_reg_test$sample(
+  data = simdat_ls, 
+  seed = 12345, 
+  chains = 4, 
+  parallel_chains = 4) # entirely fails to run -- generates NaN values in sigma, but not sure why
+
