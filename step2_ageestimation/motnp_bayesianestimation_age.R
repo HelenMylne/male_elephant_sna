e857f6dd78611eeaf7d2fedf144a562ea407edc2
HelenMylne <hkm513@york.ac.uk>
2022-07-19 19:02:28 +0100

    merge in script from motnp_ageestimation_nodalregression -- this script needed splitting into two parts so that the age estimation produced the same output for both the nodal and dyadic regressions.

--- step2_ageestimation/motnp_bayesianestimation_age.R
+++ step2_ageestimation/motnp_bayesianestimation_age.R
@@ -9,11 +9,18 @@ library(cmdstanr)
 library(ggdist)
 library(posterior)
 library(bayesplot)
+library(rstan)
+library(igraph)
+library(LaplacesDemon)
 
 #library(ggthemes)
 #library(survival)
 #library(tidybayes)
 #library(data.table)
+
+#### load model ####
+# read in Stan model to estimate ages based on Gompertz bathtub distribution from ANP
+latent_age_ordinal_model <- cmdstan_model("models/age_estimation/motnp_elephant_latent_age_ordinal_regression_hkm_22.07.07.stan")
 
 #### simulate process of assigning age categories to elephants ####
 gompertz_bt <- function(a0, a1, c, b0, b1, age){
@@ -59,9 +66,6 @@ data.frame(elephants_ls) %>%
   xlab("Assigned age") + ylab("Age")
 
 #### fit model to simulated dataset ####
-# read in Stan model to estimate ages based on Gompertz bathtub distribution from ANP
-latent_age_ordinal_model <- cmdstan_model("models/age_estimation/elephant_latent_age_ordinal_regression_hkm_22.07.07.stan")
-
 #Fit model with cmdstanr
 age_estimation_fit <- latent_age_ordinal_model$sample(
   data = elephants_ls, 
@@ -129,7 +133,7 @@ age_motnp_fit <- latent_age_ordinal_model$sample(
   iter_sampling = 2000
 )
 
-# Examine the estimates. We can plot the estimated ages against the biologist assigned ages and ...
+# Examine the estimates
 age_est_mat <- age_motnp_fit$summary()[(N_motnp+2):(N_motnp*2+1), ]
 summary(age_est_mat)
 hist(age_est_mat$mean)
@@ -178,3 +182,177 @@ df %>% ggplot(aes(x=true_age, y=value, group=factor(ID))) +
   geom_hline(yintercept=c(5,10,15,20,25,40,60), linetype="dashed", alpha=0.6) +
   theme_bw() + 
   xlab("Assigned age") + ylab("Modelled age")
+
+### save output
+colnames(true_ages) <- motnp_males$id
+saveRDS(true_ages, file = 'data_processed/motnp_ageestimates_mcmcoutput_22.07.13.rds')
+
+#### recreate network plots with new ages ####
+draws_motnp2.2 <- readRDS('data_processed/motnp_bayesian_edgedistributions_a2.b2_22.02.07.rds') %>% 
+  data.matrix()
+summaries <- data.frame(dyad = colnames(draws_motnp2.2[,2:106954]),
+                        min = rep(NA, ncol(draws_motnp2.2)-1),
+                        max = rep(NA, ncol(draws_motnp2.2)-1),
+                        mean = rep(NA, ncol(draws_motnp2.2)-1),
+                        median = rep(NA, ncol(draws_motnp2.2)-1),
+                        sd = rep(NA, ncol(draws_motnp2.2)-1))
+for(i in 1:nrow(summaries)){
+  summaries$min[i]    <- min(draws_motnp2.2[,i+1])
+  summaries$max[i]    <- max(draws_motnp2.2[,i+1])
+  summaries$mean[i]   <- mean(draws_motnp2.2[,i+1])
+  summaries$median[i] <- median(draws_motnp2.2[,i+1])
+  summaries$sd[i]     <- sd(draws_motnp2.2[,i+1])
+}
+
+par(mai = c(0.2,0.2,0.2,0.2))
+
+# create array of draws per dyad (distributions)
+counts_df <- read_csv('data_processed/motnp_bayesian_allpairwiseevents_splitbygrouptype_22.01.13.csv')
+adj_tensor <- array(0, c(NROW(unique(counts_df$id_1))+1,
+                         NROW(unique(counts_df$id_2))+1,
+                         NROW(draws_motnp2.2)),
+                    dimnames = list(c(unique(counts_df$id_1),'U9'),
+                                    c('F1',unique(counts_df$id_2)),
+                                    NULL))
+N <- nrow(counts_df)
+half_draws1 <- draws_motnp2.2[,2:54000]
+half_draws2 <- draws_motnp2.2[,54001:N+1]
+rm(draws_motnp2.2)
+
+for (i in 1:54000) {            # can cope with jumps of 50000 dyads at a time
+  dyad_row <- counts_df[i, ]
+  adj_tensor[dyad_row$id_1, dyad_row$id_2, ] <- half_draws1[, i]
+}
+rm(half_draws1)
+for (i in 54001:N) {
+  dyad_row <- counts_df[i, ]
+  adj_tensor[dyad_row$id_1, dyad_row$id_2, ] <- half_draws2[, i-54000]
+}
+rm(half_draws2)
+adj_tensor[,,1]
+
+# convert to array of medians and 95% credible intervals
+adj_quantiles <- apply(adj_tensor, c(1, 2), function(x) quantile(x, probs = c(0.025, 0.5, 0.975)))
+(adj_lower <- adj_quantiles[1, , ])
+(adj_mid   <- adj_quantiles[2, , ])
+(adj_upper <- adj_quantiles[3, , ])
+adj_range <- (adj_upper - adj_lower) ; adj_range[is.nan(adj_range)] <- 0
+rm(adj_tensor, adj_quantiles, adj_lower, adj_upper, dyad_row)
+
+# Generate two igraph objects, one from the median and one from the standardised width.
+g_mid <- graph_from_adjacency_matrix(adj_mid,   mode="undirected", weighted=TRUE)
+g_rng <- graph_from_adjacency_matrix(adj_range, mode="undirected", weighted=TRUE)
+
+# Generate nodes data for plotting characteristics
+ages <- as.matrix(true_ages)
+motnp_males$age_mean <- NA
+for(i in 1:nrow(motnp_males)){
+  motnp_males$age_mean[i] <- mean(as.numeric(ages[i]))
+}
+
+# create variables for different degrees of node connectedness
+motnp_males$degree_0.1 <- NA
+motnp_males$degree_0.2 <- NA
+motnp_males$degree_0.3 <- NA
+motnp_males$degree_0.4 <- NA
+motnp_males$degree_0.5 <- NA
+
+summaries <- separate(summaries, dyad, c('id_1','id_2'), '_', F)
+for(i in 1:NROW(motnp_males)){
+  rows <- summaries[summaries$id_1 == motnp_males$id[i] | summaries$id_2 == motnp_males$id[i],]
+  motnp_males$degree_0.1[i] <- length(which(rows$median > 0.1))
+  motnp_males$degree_0.2[i] <- length(which(rows$median > 0.2))
+  motnp_males$degree_0.3[i] <- length(which(rows$median > 0.3))
+  motnp_males$degree_0.4[i] <- length(which(rows$median > 0.4))
+  motnp_males$degree_0.5[i] <- length(which(rows$median > 0.5))
+}
+
+which(motnp_males$degree_0.1 < motnp_males$degree_0.2)
+which(motnp_males$degree_0.2 < motnp_males$degree_0.3)
+which(motnp_males$degree_0.3 < motnp_males$degree_0.4)
+which(motnp_males$degree_0.4 < motnp_males$degree_0.5)
+
+### create data frame of all nodes to subset out males
+all_eles <- counts_df[,c(2,4,12,14,16,18,22)] %>% distinct()
+u9 <- counts_df[counts_df$id_2 == 'U9',c(3,5,13,15,17,19,23)] %>% distinct()
+colnames(all_eles) <- c("id","node","name","age_class","age_category","sex","dem_class")
+colnames(u9) <- c("id","node","name","age_class","age_category","sex","dem_class")
+all_eles <- rbind(all_eles, u9) ; rm(u9)
+
+males <- all_eles[all_eles$id %in% motnp_males$id,]
+females <- anti_join(all_eles, males)
+
+### create small df for plotting
+plot_males <- motnp_males[,c('id','degree_0.3','age_mean','count')]
+plot_males <- left_join(males, plot_males, by = 'id')
+
+### All males
+g_mid_m <- delete.vertices(graph = g_mid,
+                           v = all_eles$id[all_eles$id %in% females$id])
+g_rng_m <- delete.vertices(graph = g_rng,
+                           v = all_eles$id[all_eles$id %in% females$id])
+coords_m <- layout_nicely(g_mid_m)
+plot(g_mid_m,
+     edge.color = rgb(0,0,0,0.25),
+     edge.width = E(g_rng_m)$weight,
+     vertex.size = 7,
+     vertex.label = NA,
+     layout = coords_m)
+plot(g_mid_m,
+     edge.width = E(g_mid_m)$weight,
+     edge.color = 'black',
+     vertex.size = 7,
+     vertex.label = plot_males$id,
+     vertex.label.color = 'black',
+     vertex.label.family = 'Helvetica',
+     vertex.label.cex = 0.5,
+     vertex.label.dist = 0,
+     vertex.color = plot_males$age_mean,
+     layout = coords_m, add = TRUE)
+
+### Only males degree > 0.3
+males0.3 <- plot_males[plot_males$degree_0.3 > 0,]
+g_mid_m0.3 <- delete.vertices(graph = g_mid_m,
+                              v = plot_males$id[plot_males$degree_0.3 == 0])
+g_rng_m0.3 <- delete.vertices(graph = g_rng_m,
+                              v = plot_males$id[plot_males$degree_0.3 == 0])
+
+coords_m0.3 <- layout_nicely(g_mid_m0.3)
+plot(g_mid_m0.3,
+     edge.width = ifelse(E(g_mid_m0.3)$weight > 0.3,
+                         E(g_rng_m0.3)$weight,0),
+     edge.color = rgb(0,0,0,0.25),
+     vertex.size = 7,
+     vertex.label = NA,
+     layout = coords_m0.3)
+plot(g_mid_m0.3,
+     edge.width = ifelse(E(g_mid_m0.3)$weight > 0.3,
+                         E(g_mid_m0.3)$weight, 0),
+     edge.color = 'black',
+     vertex.size = 7,
+     vertex.label.color = 'black',
+     vertex.label.family = 'Helvetica',
+     vertex.label.cex = 0.5,
+     vertex.label.dist = 0,
+     vertex.color = males0.3$age_mean,
+     layout = coords_m0.3, add = TRUE)
+
+plot(g_mid_m0.3,
+     edge.width = ifelse(E(g_mid_m0.3)$weight > 0.3,
+                         E(g_rng_m0.3)$weight,0),
+     edge.color = rgb(0,0,0,0.25),
+     vertex.size = 1,
+     vertex.label = NA,
+     layout = coords_m0.3)
+plot(g_mid_m0.3,
+     edge.width = ifelse(E(g_mid_m0.3)$weight > 0.3,
+                         E(g_mid_m0.3)$weight, 0),
+     edge.color = 'black',
+     vertex.size = males0.3$count,
+     vertex.label.color = 'black',
+     vertex.label.family = 'Helvetica',
+     vertex.label.cex = 0.5,
+     vertex.label.dist = 0,
+     vertex.color = males0.3$age_mean,
+     layout = coords_m0.3, add = TRUE)
+
