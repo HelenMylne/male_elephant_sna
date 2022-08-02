41b8dfd9c9bb20d7aa39610146ffaac3c3291b3c
HelenMylne <83633954+HelenMylne@users.noreply.github.com>
2022-08-02 15:17:49 +0100

    Update test_script_researchcluster.R

--- test_script_researchcluster.R
+++ test_script_researchcluster.R
@@ -1,7 +1,7 @@
 # file to test whether sourcing a script directly from GitHub to research cluster will work
 
-library(tidyverse)
-print('tidyverse loaded')
+#library(tidyverse)
+#print('tidyverse loaded')
 
 test_id <- read_csv('data_processed/motnp_id.csv')
 head(test_id)
