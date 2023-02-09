d09afad36391f76728f39697a772ee36687c6fff
HelenMylne <hkm513@york.ac.uk>
2023-02-09 18:48:35 +0000

    Update motnp_simulate_randomness.R
    might have worked out a reasonable way of simulating edge weights?

--- step3_edgeweightestimation/motnp_simulate_randomness.R
+++ step3_edgeweightestimation/motnp_simulate_randomness.R
@@ -34,8 +34,8 @@ ew$node_2 <- as.numeric(ew$node_2)
 hist(LaplacesDemon::invlogit(motnp_edge_weights_strongpriors$edge_samples))
 counts_df$sri <- counts_df$event_count / counts_df$count_dyad
 hist(counts_df$sri)
-a <- mean(counts_df$sri[counts_df$sri > 0] )
-e <-  sd( counts_df$sri[counts_df$sri > 0] )
+(a <- mean(counts_df$sri[counts_df$sri > 0] ))
+(e <-  sd( counts_df$sri[counts_df$sri > 0] ))
 alpha <- rbeta(nrow(ew), a, e)
 hist(alpha)
 
@@ -43,130 +43,125 @@ hist(alpha)
 ew$friends <- rbinom(n = nrow(ew), size = 1, prob = 0.046)
 sum(ew$friends)/nrow(ew)
 #ew$not_friends <- 1-ew$friends
-ew$not_friends <- rbinom(n = nrow(ew), size = 1, prob = (1-0.046))
+#ew$not_friends <- rbinom(n = nrow(ew), size = 1, prob = (1-0.046))
 
 # gamma = edge weight increase created by friendship
 gamma <- 0.1
 
-# weight[i,j] = Beta(alpha+friends_prob*gamma)
-hist(rbeta(10000, 0.07, 0.0), xlim = c(0,1))
-hist(rbeta(10000, 0.07, 0.1), xlim = c(0,1))
-hist(rbeta(10000, 0.07, 0.2), xlim = c(0,1))
-hist(rbeta(10000, 0.07, 0.3), xlim = c(0,1))
-hist(rbeta(10000, 0.1, 0.1), xlim = c(0,1))
-hist(rbeta(10000, 0.2, 0.1), xlim = c(0,1))
-hist(rbeta(10000, 0.3, 0.1), xlim = c(0,1))
-hist(rbeta(10000, 0.5, 0.5), xlim = c(0,1))
-hist(rbeta(10000, 1, 1), xlim = c(0,1))
-hist(rbeta(10000, 3, 1), xlim = c(0,1))
-hist(rbeta(10000, 3, 3), xlim = c(0,1))
-hist(rbeta(10000, 1, 3), xlim = c(0,1))
-hist(rbeta(10000, 0.1, 3), xlim = c(0,1))
-hist(rbeta(10000, 1, 30), xlim = c(0,1))
+### weight[i,j] = Beta(alpha+friends_prob*gamma) ####
+## trying to find the shape parameters that give me what I want
+#hist(rbeta(10000, 0.07, 0.0), xlim = c(0,1))
+#hist(rbeta(10000, 0.07, 0.1), xlim = c(0,1))
+#hist(rbeta(10000, 0.07, 0.2), xlim = c(0,1))
+#hist(rbeta(10000, 0.07, 0.3), xlim = c(0,1))
+hist(rbeta(10000, 0.07, 1), xlim = c(0,1))
+#hist(rbeta(10000, 0.1, 0.1), xlim = c(0,1))
+#hist(rbeta(10000, 0.2, 0.1), xlim = c(0,1))
+#hist(rbeta(10000, 0.3, 0.1), xlim = c(0,1))
+#hist(rbeta(10000, 0.5, 0.5), xlim = c(0,1))
+#hist(rbeta(10000, 1, 1), xlim = c(0,1))
+#hist(rbeta(10000, 3, 1), xlim = c(0,1))
+#hist(rbeta(10000, 3, 3), xlim = c(0,1))
+#hist(rbeta(10000, 1, 3), xlim = c(0,1))
+#hist(rbeta(10000, 0.1, 3), xlim = c(0,1))
+#hist(rbeta(10000, 1, 30), xlim = c(0,1))
+
+## theory: shape1 = mean = alpha, shape2 = source of variation = gamma*ew$friends --> (0.07,0.01) or (0.07,0.00) --> (0.07,0.00)=1 so not this one
 #ew$weight <- rbeta(n = nrow(ew), shape1 = alpha, shape2 = gamma*ew$friends)
-#ew$weight <- rbeta(n = nrow(ew), shape1 = a+gamma*ew$friends, shape2 = e)
-#ew$weight <- rbeta(n = nrow(ew), shape1 = alpha+gamma*ew$friends, shape2 = e)
-# Beta(times x did happen, times x didn't happen) --> Beta(a+Bern(0.046),a+Bern(0.956))
-shapes <- simstudy::betaGetShapes(a+ew$friends*gamma, a+ew$not_friends*gamma)
+
+## theory: error in alpha = stdev in weight --> shape1 = mean = a+f*g, shape2 = e
+ew$weight <- rbeta(n = nrow(ew), shape1 = a+gamma*ew$friends, shape2 = e)
+#shapes <- simstudy::betaGetShapes(a+ew$friends*gamma, e)
+#ew$weight <- rbeta(n = nrow(ew), shape1 = shapes$shape1, shape2 = shapes$shape2)
+#hist(ew$weight, ylim = c(0,1000)) # too stong on the 1, not enough low but >0
+
+## theory: shape1 = mean, shape2 = precision = mean+sources of variation
+ew$weight <- rbeta(n = nrow(ew), shape1 = a, shape2 = a+ew$friends*gamma)
+hist(ew$weight, ylim = c(0,1000)) # too strong on extremes
+shapes <- simstudy::betaGetShapes(a, a+ew$friends*gamma)
 ew$weight <- rbeta(n = nrow(ew), shape1 = shapes$shape1, shape2 = shapes$shape2)
-hist(ew$weight, ylim = c(0,1000))
+hist(ew$weight, ylim = c(0,1000)) # WAY too strong on extremes
+ew$weight <- rbeta(n = nrow(ew), shape1 = alpha, shape2 = alpha+ew$friends*gamma)
+hist(ew$weight) # too strong on extremes, not enough variation in middle
+
+# theory: not_friends = Beta(a, not_a), friends = Beta(a+g, not a+g)
+shapes_f <- simstudy::betaGetShapes(a, 1-a)
+shapes_n <- simstudy::betaGetShapes(a+0.1, 1-(a+0.1))
+ew$weight <- ifelse(ew$friends == 0,
+                    rbeta(n = 1, shape1 = shapes_f$shape1, shape2 = shapes_f$shape2),
+                    rbeta(n = 1, shape1 = shapes_n$shape1, shape2 = shapes_n$shape2))
+hist(ew$weight) # entirely at extremes, mostly 1
+
+## theory: Beta(times Z did happen, times Z didn't happen) --> shape1 = mean+g*friends = a+Bern(0.046), shape2 = mean+g*not_friends = a+Bern(0.956))
+ew$weight <- rbeta(n = nrow(ew),
+                   shape1 = a+rbinom(1,1,0.046),
+                   shape2 = a+rbinom(1,1,0.956))
+hist(ew$weight)
+# don't alter shapes here because these are actual shape parameters rather than mean/precision values
+#shapes <- simstudy::betaGetShapes(a+rbinom(1,1,0.046), a+rbinom(1,1,0.956))
+#ew$weight <- rbeta(n = nrow(ew), shape1 = shapes$shape1, shape2 = shapes$shape2)
+#hist(ew$weight)
 
 # loop through i and j (where j starts at i so only doing top half of matrix) { if w[i,j] ≤ alpha { if Bernoulli(0.8) = 0 then w[i,j] = 0 }} --> then transpose to bottom half of matrix
+ew_mat <- matrix(data = NA, nrow = N, ncol = N, dimnames = list(x = 1:N, y = 1:N))
+for(i in 1:N){
+  for(j in i:N){
+    if( i == j ){ ew_mat[i,j] <- 1 }
+    else{
+      weight <- ew$weight[which(ew$node_1 == colnames(ew_mat)[i] & ew$node_2 == rownames(ew_mat)[j])]
+      if( weight <= a ){
+        w <- weight*rbinom(n = 1, size = 1, prob = 0.8)
+      }
+      else{ w <- weight }
+      ew_mat[i,j] <- w
+    }
+  }
+}
+for(i in 1:N) {
+  for(j in 1:i) {
+    ew_mat[i,j] = ew_mat[j,i]
+  }
+}
 
+## build gbi matrix -- first need matrix of raw association data
+assn <- matrix(data = 0, nrow = N, ncol = N,
+               dimnames = list(x = 1:N, y = 1:N))
+for(i in 1:N){
+  for(j in i:N){
+    assn[i,j] <- rbinom(1,50,prob = ew_mat[i,j]) # all dyads observed 50 times total
+  }
+}
+for(i in 1:N) {
+  for(j in 1:i) {
+    assn[i,j] = assn[j,i]
+  }
+}
+ew$together <- NA
+ew$apart <- NA
+for(i in 1:nrow(ew)){
+  ew$together[i] <- assn[rownames(assn) == ew$node_1[i], colnames(assn) == ew$node_2[i]]
+  ew$apart[i] <- 50-ew$together[i]
+}
+
+motnp <- read_csv('../data_processed/motnp_eles_long.csv')
+N_sightings <- length(unique(motnp$encounter[motnp$type != 'BH']))
+gbi_sim <- matrix(data = 0, nrow = N_sightings, ncol = N,
+                  dimnames = list(x = 1:N_sightings, y = 1:N))
 
 # run permutations and see if it says network is non-random
-
-
+N_networks <- 10000
+random_networks <- asnipe::network_permutation(association_data = gbi_sim,
+                                               association_matrix = ew_mat,
+                                               permutations = N_networks)
+cv_random_networks <- rep(0,N_networks)   # generate empty vector to fill with cv values
+for (i in c(1:N_networks)) {
+  net_rand <- random_networks[i,,]
+  cv_random_networks[i] <- raster::cv(net_rand)
+  if(i %% 1000 == 0) {print(i)}
+}
 
 #### side note: what is CV output when SRI ≠ 1 or 0? #####
 counts_df$sri
 sri <- counts_df$sri[counts_df$sri > 0 & counts_df$sri < 1]
 raster::cv(sri)
-
-#### aninet non-random variation test ####
-library(aninet)
-
-### generate gbi_matrix
-eles <- read_delim(file = '../data_processed/motnp_eles_long.csv', delim = ',')
-eles$location <- paste(eles$gps_s, eles$gps_e, sep = '_') # make single variable for unique locations
-eles <- eles[,c(1,16,2,3,17,4,5,14,7,8,10,13)]            # rearrange variables
-nodes <- read_delim(file = '../data_processed/motnp_elenodes.csv', delim = ',') # read in node data
-eles_asnipe <- eles[,c(3,4,2,5)]                                       # date, time, elephant, location
-eles_asnipe$Date <- as.integer(eles_asnipe$date)                       # make date numeric
-eles_asnipe$Date <- 1+eles_asnipe$Date - min(eles_asnipe$Date)         # start from 1, not 1st January 1970
-eles_asnipe$Time <- ifelse(eles_asnipe$time > 1, NA, eles_asnipe$time) # time = proportion of day so anything >1 has to be wrong
-eles_asnipe$Time <- eles_asnipe$Time*(24*60*60)                        # convert time values to seconds through day
-eles_asnipe <- eles_asnipe[,c(5,6,3,4)]                                # create data frame to produce gbi matrix from
-colnames(eles_asnipe) <- c('Date','Time','ID','Location')              # rename variables for get_gbi
-eles_asnipe$ID <- as.character(eles_asnipe$ID)                         # correct data type
-eles_asnipe$d_pad <- str_pad(eles_asnipe$Date, 3, pad = '0')           # 0-pad dates
-eles_asnipe$encounter <- paste(eles_asnipe$d_pad, eles_asnipe$Time, eles_asnipe$Location, sep = '_') # unique value for each sighting
-eles_asnipe$group <- as.integer(as.factor(eles_asnipe$encounter)) # unique factor for every sighting
-eles_asnipe <- eles_asnipe[,c(3,7)]            # create data table for gbi matrix
-eles_asnipe <- data.table::setDT(eles_asnipe)  # create data table for gbi matrix
-gbi_matrix <- spatsoc::get_gbi(DT = eles_asnipe, group = 'group', id = 'ID')  # create gbi matrix
-motnp_ages <- readRDS('../data_processed/motnp_ageestimates_mcmcoutput.rds') %>%
-  pivot_longer(cols = everything(), names_to = 'id', values_to = 'age')
-ids <- unique(c(counts_df$id_1, counts_df$id_2))
-gbi_males <- gbi_matrix[,colnames(gbi_matrix) %in% ids]
-gbi_males <- gbi_males[rowSums(gbi_males) > 0,] # remove male-only sightings
-
-( num_permutations <- aninet::gbi_MCMC_iters(gbi = gbi_males, target_samples = 1000, quiet = FALSE) )
-
-permute_gbi <- gbi_MCMC(
-  data = gbi_males,
-  ind_constraint = NULL,
-  group_constraint = NULL,
-  samples = num_permutations,
-  thin = 100,
-  burnin = 1000,
-  chains = 4,
-  FUN = NULL      # leaving as NULL here will mean that permute_gbi$FUN(gbi_males) below gives CV, Mean, SD and SRI
-)
-str(permute_gbi)
-permute_gbi$FUN(gbi_males)
-permute_gbi$permuted_data
-
-
-#### ignore all this -- I thought the permutations needed to be on SRI data but nope it just wants the GBI matrix ####
-counts_df$sri <- counts_df$event_count / counts_df$count_dyad
-sri_males <- matrix(data = NA, nrow = length(ids), ncol = length(ids), dimnames = list(x = ids, y = ids))
-for(i in 1:length(ids)){
-  for(j in 1:length(ids)){
-    if( i == j ) { sri_males[i,j] <- 0 }
-    else {
-      if( i < j ){
-        sri_males[i,j] <- counts_df$sri[which(counts_df$id_1 == rownames(sri_males)[i] &
-                                                counts_df$id_2 == colnames(sri_males)[j])]
-      }
-        else {
-          sri_males[i,j] <- counts_df$sri[which(counts_df$id_1 == rownames(sri_males)[j] &
-                                                  counts_df$id_2 == colnames(sri_males)[i])]
-      }
-    }
-  }
-}
-
-( num_permutations <- aninet::gbi_MCMC_iters(gbi = sri_males, target_samples = 1000, quiet = FALSE) )
-
-permute_network <- gbi_MCMC(
-  data = sri_males,
-  ind_constraint = NULL,
-  group_constraint = NULL,
-  samples = num_permutations,
-  thin = 100,
-  burnin = 1000,
-  chains = 4,
-  FUN = NULL      # leaving as NULL here will mean that permute_network$FUN(gbi_males) below gives CV, Mean, SD and SRI
-)
-str(permute_network)
-permute_network$FUN(gbi_males)
-permute_network$permuted_data
-
-
-
-
-
-
-
 
