95152e8b83f18b6225debcd56df7a1eb01dbc4bd
HelenMylne <hkm513@york.ac.uk>
2023-03-19 19:26:02 +0000

    Update mpnp_bisonr_edgeweight_plots.R
    comment out short time windows for running long one on viking, add proper set up for long time window so it actually works

--- step3_edgeweightestimation/mpnp_bisonr_edgeweight_plots.R
+++ step3_edgeweightestimation/mpnp_bisonr_edgeweight_plots.R
@@ -29,97 +29,108 @@ set.seed(12345)
 #set_cmdstan_path('H:/rlibs/4.2.1/')
 
 #### short time windows ####
-max_time_window <- 5
-for( time_window in 1:max_time_window){
+#max_time_window <- 5
+#for( time_window in 1:max_time_window){
   ## set up ####
   ### add time marker
-  print(paste0('start window ', time_window ,' at ', Sys.time()))
+#  print(paste0('start window ', time_window ,' at ', Sys.time()))
   
   # create output pdf
-  pdf(file = paste0('../outputs/mpnp',time_window,'_bisonr_plotnetwork.pdf'))
+#  pdf(file = paste0('../outputs/mpnp',time_window,'_bisonr_plotnetwork.pdf'))
   
   ### load data
-  load(paste0('mpnp_edgecalculations/mpnpshort',time_window,'_bisonr_edgescalculated.RData'))
-  rm(counts, draws, dyads, ew_chain, ew_edgelist, global_cv, mpnp_edges_null, priors, summary, draw98, filename, median98, sri98, model_averaging) ; gc()
+#  load(paste0('mpnp_edgecalculations/mpnpshort',time_window,'_bisonr_edgescalculated.RData'))
+#  rm(counts, draws, dyads, ew_chain, ew_edgelist, global_cv, mpnp_edges_null, priors, summary, draw98, filename, median98, sri98, model_averaging) ; gc()
   
   ## plot network ####
   # create nodes data frame
-  nodes <- data.frame(node = sort(unique(c(counts_df_model$node_1_id,
-                                           counts_df_model$node_2_id))),
-                      id = NA,
-                      age = NA,
-                      sightings = NA)
-    for(i in 1:nrow(nodes)){
-    df <- counts_df[(counts_df$period == time_window & counts_df$node_1 == nodes$node[i]),
-                    c('id_1','count_period_1')] %>% distinct()
-    if(nrow(df) > 0){
-      nodes$sightings[i] <- df$count_period_1[1]
-      nodes$id[i] <- df$id_1[1]
-    }
-    else {
-      df <- counts_df[(counts_df$period == time_window & counts_df$node_2 == nodes$node[i]),
-                      c('id_2','count_period_2')] %>% distinct()
-      nodes$sightings[i] <- df$count_period_2[1]
-      nodes$id[i] <- df$id_2[1]
-    }
-    nodes$age[i] <- mean(mpnp_ages$age[which(mpnp_ages$id == nodes$id[i])])
-    rm(df)
-  }
-  nodes$sightings <- as.numeric(nodes$sightings)
-  str(nodes)
-  
-  # plot network -- 0.05
-  plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.05,
-                         vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
-                         vertex.label.color2 = 'black', vertex.color2 = nodes$age,
-                         vertex.size2 = nodes$sightings, edge.color2 = 'black') 
-  plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.05,
-                          node.size = nodes, node.colour = nodes, lwd = 10)
-  
-  # plot network -- 0.1
-  plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.1,
-                         vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
-                         vertex.label.color2 = 'black', vertex.color2 = nodes$age,
-                         vertex.size2 = nodes$sightings, edge.color2 = 'black') 
-  plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.1,
-                          node.size = nodes, node.colour = nodes, lwd = 10)
-  
-  # plot network -- 0.15
-  if(length(which(edgelist$median > 0.15)) > 0){
-    plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.15,
-                           vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
-                           vertex.label.color2 = 'black', vertex.color2 = nodes$age,
-                           vertex.size2 = nodes$sightings, edge.color2 = 'black') 
-    plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.15,
-                            node.size = nodes, node.colour = nodes, lwd = 10)
-  }
+#  nodes <- data.frame(node = sort(unique(c(counts_df_model$node_1_id,
+#                                           counts_df_model$node_2_id))),
+#                      id = NA,
+#                      age = NA,
+#                      sightings = NA)
+#    for(i in 1:nrow(nodes)){
+#    df <- counts_df[(counts_df$period == time_window & counts_df$node_1 == nodes$node[i]),
+#                    c('id_1','count_period_1')] %>% distinct()
+#    if(nrow(df) > 0){
+#      nodes$sightings[i] <- df$count_period_1[1]
+#      nodes$id[i] <- df$id_1[1]
+#    }
+#    else {
+#      df <- counts_df[(counts_df$period == time_window & counts_df$node_2 == nodes$node[i]),
+#                      c('id_2','count_period_2')] %>% distinct()
+#      nodes$sightings[i] <- df$count_period_2[1]
+#      nodes$id[i] <- df$id_2[1]
+#    }
+#    nodes$age[i] <- mean(mpnp_ages$age[which(mpnp_ages$id == nodes$id[i])])
+#    rm(df)
+#  }
+#  nodes$sightings <- as.numeric(nodes$sightings)
+#  str(nodes)
+#  
+#  # plot network -- 0.05
+#  plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.05,
+#                         vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+#                         vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+#                         vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+#  plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.05,
+#                          node.size = nodes, node.colour = nodes, lwd = 10)
+#  
+#  # plot network -- 0.1
+#  plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.1,
+#                         vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+#                         vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+#                         vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+#  plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.1,
+#                          node.size = nodes, node.colour = nodes, lwd = 10)
+#  
+#  # plot network -- 0.15
+#  if(length(which(edgelist$median > 0.15)) > 0){
+#    plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.15,
+#                           vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+#                           vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+#                           vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+#    plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.15,
+#                            node.size = nodes, node.colour = nodes, lwd = 10)
+#  }
   
   # plot network -- 0.2
-  if(length(which(edgelist$median > 0.2)) > 0){
-    plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.2,
-                           vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
-                           vertex.label.color2 = 'black', vertex.color2 = nodes$age,
-                           vertex.size2 = nodes$sightings, edge.color2 = 'black') 
-    plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.2,
-                            node.size = nodes, node.colour = nodes, lwd = 10)
-  }
+#  if(length(which(edgelist$median > 0.2)) > 0){
+#    plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.2,
+#                           vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+#                           vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+#                           vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+#    plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.2,
+#                            node.size = nodes, node.colour = nodes, lwd = 10)
+#  }
   
   ### add time marker
-  print(paste0('network plots completed at ', Sys.time()))
-  
+#  print(paste0('network plots completed at ', Sys.time()))
+
 ## clean up ####
 ### save pdf
-dev.off()
+#dev.off()
 
 ### clear environment
-rm(list = ls()[!(ls() %in% 'time_window')])
+#rm(list = ls()[!(ls() %in% 'time_window')])
 
 ### add time marker
-print(paste0('time window ', time_window, ' completed at ', Sys.time()))
+#print(paste0('time window ', time_window, ' completed at ', Sys.time()))
 
-}
+#}
 
 #### long window ####
+## set up ####
+### add time marker
+print(paste0('start long window at ', Sys.time()))
+
+# create output pdf
+pdf('../outputs/mpnplongwindow_bisonr_plotnetwork.pdf')
+
+### load data
+load('mpnp_edgecalculations/mpnp_longwindow_bisonr_edgescalculated.RData')
+rm(counts, draws, dyads, ew_chain, ew_edgelist, global_cv, mpnp_edges_null, priors, summary, draw98, filename, median98, sri98, model_averaging) ; gc()
+
 # create nodes data frame
 nodes <- data.frame(bull = sort(unique(ages$id)),
                     age = NA,
@@ -135,13 +146,41 @@ for(i in 1:nrow(nodes)){
 nodes$sightings <- as.numeric(nodes$sightings)
 str(nodes)
 
-# plot network
-plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.15,
+# plot network -- 0.05
+plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.05,
                        vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
                        vertex.label.color2 = 'black', vertex.color2 = nodes$age,
-                       vertex.size2 = nodes$sightings, edge.color2 = 'black')
-plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.15,
+                       vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.05,
                         node.size = nodes, node.colour = nodes, lwd = 10)
+
+# plot network -- 0.1
+plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.1,
+                       vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+                       vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+                       vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.1,
+                        node.size = nodes, node.colour = nodes, lwd = 10)
+
+# plot network -- 0.15
+if(length(which(edgelist$median > 0.15)) > 0){
+  plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.15,
+                         vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+                         vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+                         vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+  plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.15,
+                          node.size = nodes, node.colour = nodes, lwd = 10)
+}
+
+# plot network -- 0.2
+if(length(which(edgelist$median > 0.2)) > 0){
+  plot_network_threshold(mpnp_edge_weights, lwd = 2, ci = 0.9, threshold = 0.2,
+                         vertex.label.color1 = NA, edge.color1 = rgb(0,0,0,0.25),
+                         vertex.label.color2 = 'black', vertex.color2 = nodes$age,
+                         vertex.size2 = nodes$sightings, edge.color2 = 'black') 
+  plot_network_threshold2(obj = mpnp_edge_weights, threshold = 0.2,
+                          node.size = nodes, node.colour = nodes, lwd = 10)
+}
 
 ### add time marker
 print(paste0('network plots completed at ', Sys.time()))
