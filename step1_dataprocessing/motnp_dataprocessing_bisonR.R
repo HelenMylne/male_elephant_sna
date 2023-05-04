772361acca300901cd254db3e77900729f34c66f
HelenMylne <hkm513@york.ac.uk>
2023-05-04 18:43:03 +0100

    ready for Viking

--- step1_dataprocessing/motnp_dataprocessing_bisonR.R
+++ step1_dataprocessing/motnp_dataprocessing_bisonR.R
@@ -12,13 +12,15 @@
 #library(zoo)        # sort date columns out
 #library(asnipe)     # generating networks
 
-library(tidyverse, lib.loc = '../packages')
-library(lubridate, lib.loc = '../packages')
-library(janitor, lib.loc = '../packages')
-library(hms, lib.loc = '../packages')
-library(readxl, lib.loc = '../packages')
-library(data.table, lib.loc = '../packages')
-library(spatsoc, lib.loc = '../packages')
+library(tidyverse, lib.loc = '../packages/')
+library(lubridate, lib.loc = '../packages/')
+library(janitor, lib.loc = '../packages/')
+library(hms, lib.loc = '../packages/')
+library(readxl, lib.loc = '../packages/')
+library(data.table, lib.loc = '../packages/')
+library(spatsoc, lib.loc = '../packages/')
+library(cmdstanr, lib.loc = '../packages/')
+library(bisonR, lib.loc = '../packages/')
 
 #### import data ####
 # elephant encounters
@@ -57,70 +59,70 @@ gbi_matrix <- spatsoc::get_gbi(DT = eles_asnipe, group = 'group', id = 'ID')  # 
 
 #### old version ####
 ### code to convert gbi matrix format to dyadic data frame, shared by Prof Dan Franks and available from @JHart96 GitHub repository (https://github.com/JHart96/bison_examples/blob/main/examples/convert_gbi.md) -- NOTE: this step takes ~ 1.5 days to run
-gbi_df <- data.frame(node_1 = numeric(), node_2 = numeric(), social_event = numeric(), obs_id = numeric()) # create empty data frame
-for (obs_id in 1:nrow(gbi_matrix)) {              # run through every sighting in gbi matrix
-  for (i in which(gbi_matrix[obs_id, ] == 1)) {   # for that sighting, take every individual that was present
-    for (j in 1:ncol(gbi_matrix)) {               # run through every other elephant and record if they were also present
-      if (i != j) {
-        # make sure node_1 < node_2: not necessary but makes things a bit easier
-        if (i < j) {
-          node_1 <- i
-          node_2 <- j
-        } else {
-          node_1 <- j
-          node_2 <- i
-        }
-        gbi_df[nrow(gbi_df) + 1, ] <- list(node_1 = node_1,
-                                           node_2 = node_2,
-                                           social_event = (gbi_matrix[obs_id, i] == gbi_matrix[obs_id, j]),
-                                           obs_id = obs_id) # add a row to gbi_df data frame containing 1/0 for both individuals present or only reference one
-      }
-    }
-  }
-  if(obs_id %% 10 == 0) {print(obs_id)}
-  if(obs_id %% 10 == 0) {print(Sys.time())}
-}
-gbi_df # check structure of gbi_df
-write_delim(gbi_df, '../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_bernoullipairwiseevents.csv', delim = ',')
+#gbi_df <- data.frame(node_1 = numeric(), node_2 = numeric(), social_event = numeric(), obs_id = numeric()) # create empty data frame
+#for (obs_id in 1:nrow(gbi_matrix)) {              # run through every sighting in gbi matrix
+#  for (i in which(gbi_matrix[obs_id, ] == 1)) {   # for that sighting, take every individual that was present
+#    for (j in 1:ncol(gbi_matrix)) {               # run through every other elephant and record if they were also present
+#      if (i != j) {
+#        # make sure node_1 < node_2: not necessary but makes things a bit easier
+#        if (i < j) {
+#          node_1 <- i
+#          node_2 <- j
+#        } else {
+#          node_1 <- j
+#          node_2 <- i
+#        }
+#        gbi_df[nrow(gbi_df) + 1, ] <- list(node_1 = node_1,
+#                                           node_2 = node_2,
+#                                           social_event = (gbi_matrix[obs_id, i] == gbi_matrix[obs_id, j]),
+#                                           obs_id = obs_id) # add a row to gbi_df data frame containing 1/0 for both individuals present or only reference one
+#      }
+#    }
+#  }
+#  if(obs_id %% 10 == 0) {print(obs_id)}
+#  if(obs_id %% 10 == 0) {print(Sys.time())}
+#}
+#gbi_df # check structure of gbi_df
+#write_delim(gbi_df, '../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_bernoullipairwiseevents.csv', delim = ',')
 # test <- read_delim('../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_bernoullipairwiseevents.csv', delim = ',')  # check that this has worked because you don't want to have to run it again!
 
 ### add elephant ID numbers to assigned index factors
-str(gbi_df)
-gbi_id <- data.frame(id_1 = colnames(gbi_matrix), node_1 = as.numeric(1:472))
-gbi_check <- left_join(x = gbi_df, y = gbi_id, by = 'node_1')
-gbi_id <- data.frame(id_2 = colnames(gbi_matrix), node_2 = as.numeric(1:472))
-gbi_check <- left_join(x = gbi_check, y = gbi_id, by = 'node_2')
-
+#str(gbi_df)
+#gbi_id <- data.frame(id_1 = colnames(gbi_matrix), node_1 = as.numeric(1:472))
+#gbi_check <- left_join(x = gbi_df, y = gbi_id, by = 'node_1')
+#gbi_id <- data.frame(id_2 = colnames(gbi_matrix), node_2 = as.numeric(1:472))
+#gbi_check <- left_join(x = gbi_check, y = gbi_id, by = 'node_2')
+#
 ### correct obs_id to match encounter numbers in other spreadsheets (some encounters have missing data: encounter numbers 1,2,3,5,6,8... where obs_id 1,2,3,4,5,6...). Warning: this step can take quite a lot of memory - may need to clear some things from environment
-eles$obs_id <- as.integer(as.factor(eles$encounter)) ; eles <- eles[,c(1,13,2:12)] # create factor variable per encounter
-gbi_encounter <- left_join(x = gbi_check, y = eles, by = 'obs_id')  # add sighting information to gbi_df
-length(unique(gbi_encounter$encounter)) # 574 -- correct
+#eles$obs_id <- as.integer(as.factor(eles$encounter)) ; eles <- eles[,c(1,13,2:12)] # create factor variable per encounter
+#gbi_encounter <- left_join(x = gbi_check, y = eles, by = 'obs_id')  # add sighting information to gbi_df
+#length(unique(gbi_encounter$encounter)) # 574 -- correct
 
 ### remove duplicate rows where an dyad is recording as being observed together twice during the same sighting
-gbi_encounter$unique <- paste(gbi_encounter$node_1, gbi_encounter$node_2, gbi_encounter$social_event, gbi_encounter$obs_id, sep = '_') # create unique variable for every dyad and encounter
-gbi_distinct <- dplyr::distinct(gbi_encounter) # 40708781 obs
-colnames(gbi_distinct) # "node_1","node_2","social_event","obs_id","id_1","id_2","encounter","elephant","date","time","location","gps_s","gps_e",'herd_type","total_elephants_numeric","total_elephants_uncert","total_id_hkm","perc_id_hkm","unique"
-head(gbi_distinct, 10)
-gbi_distinct <- gbi_distinct[,c(1:7,9:18)]                                   # rearrange columns
-colnames(gbi_distinct)[c(7,16:17)] <- c('encounter_id','total_id','perc_id') # rename columns
+#gbi_encounter$unique <- paste(gbi_encounter$node_1, gbi_encounter$node_2, gbi_encounter$social_event, gbi_encounter$obs_id, sep = '_') # create unique variable for every dyad and encounter
+#gbi_distinct <- dplyr::distinct(gbi_encounter) # 40708781 obs
+#colnames(gbi_distinct) # "node_1","node_2","social_event","obs_id","id_1","id_2","encounter","elephant","date","time","location","gps_s","gps_e",'herd_type","total_elephants_numeric","total_elephants_uncert","total_id_hkm","perc_id_hkm","unique"
+#head(gbi_distinct, 10)
+#gbi_distinct <- gbi_distinct[,c(1:7,9:18)]                                   # rearrange columns
+#colnames(gbi_distinct)[c(7,16:17)] <- c('encounter_id','total_id','perc_id') # rename columns
 
 ### convert to Bernoulli model data format -- can't actually use Bernoulli as would require too much computing power
-gbi_distinct$dyad <- paste(gbi_distinct$id_1, gbi_distinct$id_2, sep = '_') # create character variable unique to each dyad
-gbi_distinct$dyad_id <- as.integer(as.factor(gbi_distinct$dyad))            # create integer variable unique to each dyad
-gbi_distinct$location_id <- as.integer(as.factor(gbi_distinct$location))    # create integer variable unique to each location
-gbi_distinct <- dplyr::distinct(gbi_distinct)                               # remove any duplicate sightings of same individual at same place and time
-head(gbi_distinct)
+#gbi_distinct$dyad <- paste(gbi_distinct$id_1, gbi_distinct$id_2, sep = '_') # create character variable unique to each dyad
+#gbi_distinct$dyad_id <- as.integer(as.factor(gbi_distinct$dyad))            # create integer variable unique to each dyad
+#gbi_distinct$location_id <- as.integer(as.factor(gbi_distinct$location))    # create integer variable unique to each location
+#gbi_distinct <- dplyr::distinct(gbi_distinct)                               # remove any duplicate sightings of same individual at same place and time
+#head(gbi_distinct)
 
-write_delim(gbi_distinct, '../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_bernoullipairwiseevents_infoadded.csv', delim = ',')
+#write_delim(gbi_distinct, '../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_bernoullipairwiseevents_infoadded.csv', delim = ',')
 #write_delim(gbi_distinct, '../data_processed/motnp_bayesian_bernoullipairwiseevents_infoadded.csv', delim = ',')
 
-### convert to Binomial model data format -- aggregate all sightings of each dyad together into a count
-df_agg <- gbi_distinct %>%
-  group_by(id_1, id_2) %>%  # group by dyad pair
-  summarise(event_count=sum(social_event), dyad_id=cur_group_id()) %>% # count total number of times dyad were seen together
-  mutate(node_1_id=as.integer(as.factor(id_1)), node_2_id=as.integer(as.factor(id_2))) # record node IDs
-length(df_agg$id_1) == cumsum(1:471)[471] # check have correct number of dyads -- number will be the (n-1)th value of the triangular number sequence in which n = total number of elephants in analysis (472). If TRUE, correct number of pairs.
-head(df_agg) ; tail(df_agg)
+### convert to Binomial model data format -- aggregate all sightings of each dyad together into a count#
+#df_agg <- gbi_distinct %>%
+#  group_by(id_1, id_2) %>%  # group by dyad pair
+#  summarise(event_count=sum(social_event), dyad_id=cur_group_id()) %>% # count total number of times dyad were seen together
+#  mutate(node_1_id=as.integer(as.factor(id_1)), node_2_id=as.integer(as.factor(id_2))) # record node IDs
+#length(df_agg$id_1) == cumsum(1:471)[471] # check have correct number of dyads -- number will be the (n-1)th value of the triangular number sequence in which n = total number of elephants in analysis (472). If TRUE, correct number of pairs.
+#head(df_agg) ; tail(df_agg)
 ##   id_1  id_2  event_count dyad_id node_1_id node_2_id
 ##   <chr> <chr>       <dbl>   <int>     <int>     <int>
 ## 1 F1    F10             0       1         1         1  -- both F1 and F10 have registered as elephant number 1
@@ -141,46 +143,48 @@ head(df_agg) ; tail(df_agg)
 # All good except node_1_id and node_2_id reset to 1 for every new value of id_1, so node_1_id contains nothing but "1" in every cell, and node_2_id counts all values when F1 is node_1, all-1 for F10, all-2 for F100..., only U8 and U9 when U7 is node_1, and only U9 when U8 is node_1
 
 ### correct values in node_1_id and node_2_id using factor values.
-df_agg <- df_agg[,c(1:4)]                              # select desired columns
-df_agg$node_1 <- as.integer(as.factor(df_agg$id_1))    # F1 = 1, F10 = 2, F100 = 3, F101 = 4, F102 = 5... U99 = N
-df_agg$node_2 <- as.integer(as.factor(df_agg$id_2))+1  # add 1 so starts at 2 (otherwise F10 is 2 in id_1, and 1 in id_2)
-head(df_agg,10) ; tail(df_agg,10)
+#df_agg <- df_agg[,c(1:4)]                              # select desired columns
+#df_agg$node_1 <- as.integer(as.factor(df_agg$id_1))    # F1 = 1, F10 = 2, F100 = 3, F101 = 4, F102 = 5... U99 = N
+#df_agg$node_2 <- as.integer(as.factor(df_agg$id_2))+1  # add 1 so starts at 2 (otherwise F10 is 2 in id_1, and 1 in id_2)
+#head(df_agg,10) ; tail(df_agg,10)
 
 ### add data about nodes
-colnames(nodes)
-nodes <- nodes[,c(1,3:5,9,11:13)]                      # select desired columns
-nodes$id_1 <- nodes$id ; nodes$id_2 <- nodes$id        # add columns for combining datasets
-colnames(nodes) ; colnames(df_agg)
-dyads <- left_join(x = df_agg, y = nodes, by = 'id_1') # add all information about elephant 1
-colnames(dyads)[c(2,7:15)] <- c('id_2','id_pad_1','name_1','age_class_1','age_category_1','sex_1','id1_deletecolumn','count_1','dem_class_1','deletecolumn1')
-dyads <- left_join(x = dyads, y = nodes, by = 'id_2')  # add all information about elephant 2
-colnames(dyads)[c(1,16:24)] <- c('id_1','id_pad_2','name_2','age_class_2','age_category_2','sex_2','id2_deletecolumn','count_2','dem_class_2','deletecolumn2')
-dyads <- dyads[,c(4,1,2,5,6,3,7,16,8,17,9,18,10,19,11,20,13,22,14,23)] # rearrange variables
-head(dyads)
+#colnames(nodes)
+#nodes <- nodes[,c(1,3:5,9,11:13)]                      # select desired columns
+#nodes$id_1 <- nodes$id ; nodes$id_2 <- nodes$id        # add columns for combining datasets
+#colnames(nodes) ; colnames(df_agg)
+#dyads <- left_join(x = df_agg, y = nodes, by = 'id_1') # add all information about elephant 1
+#colnames(dyads)[c(2,7:15)] <- c('id_2','id_pad_1','name_1','age_class_1','age_category_1','sex_1','id1_deletecolumn'#,'count_1','dem_class_1','deletecolumn1')
+#dyads <- left_join(x = dyads, y = nodes, by = 'id_2')  # add all information about elephant 2
+#colnames(dyads)[c(1,16:24)] <- c('id_1','id_pad_2','name_2','age_class_2','age_category_2','sex_2','id2_deletecolumn','count_2','dem_class_2','deletecolumn2')
+#dyads <- dyads[,c(4,1,2,5,6,3,7,16,8,17,9,18,10,19,11,20,13,22,14,23)] # rearrange variables
+#head(dyads)
 
 ### remove any elephants from whom their is disagreement in the different data frames regarding their names or ID numbers
-unique(gbi_distinct$id_1) # 155 females, 250 males, 65 unknowns
-unique(nodes$id_1)        # 151 females, 245 males, 66 unknowns
-length(which(is.na(dyads$id_pad_1)))                 # 2639 entries where elephants have no information
-unique(dyads$id_1[which(is.na(dyads$id_pad_1))])     # "F157" "F158" "F176" "M125" "M13"  "M138" "M21"  "M223" "M227"
-length(which(is.na(dyads$id_pad_2)))                 # 1600 entries where elephants have no information
-unique(dyads$node_2[which(is.na(dyads$id_pad_1))])   # 412 elephants -- all individuals that come after F157 in the sequence
+#unique(gbi_distinct$id_1) # 155 females, 250 males, 65 unknowns
+#unique(nodes$id_1)        # 151 females, 245 males, 66 unknowns
+#length(which(is.na(dyads$id_pad_1)))                 # 2639 entries where elephants have no information
+#unique(dyads$id_1[which(is.na(dyads$id_pad_1))])     # "F157" "F158" "F176" "M125" "M13"  "M138" "M21"  "M223" "M227"
+#length(which(is.na(dyads$id_pad_2)))                 # 1600 entries where elephants have no information
+#unique(dyads$node_2[which(is.na(dyads$id_pad_1))])   # 412 elephants -- all individuals that come after F157 in the sequence
 # all of these should be removed -- these are sightings of elephants which were deleted previously from the ele_nodes and ele_links data frames due to very high uncertainty in their identity, and so their sightings are unreliable.
 
-dyads <- dyads[dyads$id_1 != "F157" & dyads$id_1 != "F158" & dyads$id_1 != "F176" & 
-                 dyads$id_1 != "M125" & dyads$id_1 != "M13" & dyads$id_1 !=  "M138" & 
-                 dyads$id_1 != "M21" & dyads$id_1 != "M223" & dyads$id_1 != "M227", ] # remove individuals with incorrect data
-dyads <- dyads[dyads$id_2 != "F157" & dyads$id_2 != "F158" & dyads$id_2 != "F176" & 
-                 dyads$id_2 != "M125" & dyads$id_2 != "M13" & dyads$id_2 !=  "M138" & 
-                 dyads$id_2 != "M21" & dyads$id_2 != "M223" & dyads$id_2 != "M227", ] # remove individuals with incorrect data
-length(which(is.na(dyads$id_pad_1)))                 # 0 entries where elephants have no information
-length(which(is.na(dyads$id_pad_2)))                 # 0 entries where elephants have no information
+#dyads <- dyads[dyads$id_1 != "F157" & dyads$id_1 != "F158" & dyads$id_1 != "F176" & 
+#                 dyads$id_1 != "M125" & dyads$id_1 != "M13" & dyads$id_1 !=  "M138" & 
+#                 dyads$id_1 != "M21" & dyads$id_1 != "M223" & dyads$id_1 != "M227", ] # remove individuals with incorrect data
+#dyads <- dyads[dyads$id_2 != "F157" & dyads$id_2 != "F158" & dyads$id_2 != "F176" & 
+#                 dyads$id_2 != "M125" & dyads$id_2 != "M13" & dyads$id_2 !=  "M138" & 
+#                 dyads$id_2 != "M21" & dyads$id_2 != "M223" & dyads$id_2 != "M227", ] # remove individuals with incorrect data
+#length(which(is.na(dyads$id_pad_1)))                 # 0 entries where elephants have no information
+#length(which(is.na(dyads$id_pad_2)))                 # 0 entries where elephants have no information
 
 ### write csv
-readr::write_delim(dyads, '../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_binomialpairwiseevents.csv', delim = ',') # write to file
+#readr::write_delim(dyads, '../../../../Google Drive/Shared drives/Helen PhD/chapter1_age/data_processed/motnp_bayesian_binomialpairwiseevents.csv', delim = ',') # write to file
 
 #### bisonR version ####
 bison_df <- bisonR::convert_gbi_to_bison(gbi_matrix)
 
 old_df <- read_csv('../data_processed/motnp_bayesian_bernoullipairwiseevents.csv')
 which(bison_df != olf_df)
+
+save.image('motnp_datareprocessed_bisonR.RData')
