8400a70f228941c2ea0e5c3e734593ad8d75a509
HelenMylne <hkm513@york.ac.uk>
2023-06-02 10:40:32 +0100

    tidy up and update checks

--- step3_edgeweightestimation/old_versions/experimenting_with_stan.R
+++ step3_edgeweightestimation/old_versions/experimenting_with_stan.R
@@ -1,17 +1,22 @@
 #### set up ####
 # library(tidyverse) ; library(dplyr) ; library(rstan) ; library(rethinking) ; library(igraph) ; library(cmdstanr)
-library(tidyverse, lib.loc = 'packages/')
-library(dplyr, lib.loc = 'packages/')
-#library(rstan, lib.loc = 'packages/')
-library(rethinking, lib.loc = 'packages/')
-library(igraph, lib.loc = 'packages/')
-library(cmdstanr, lib.loc = 'packages/')
+library(cmdstanr)
+library(Rcpp)
+library(tidyverse, lib.loc = '../packages/')
+library(dplyr, lib.loc = '../packages/')
+library(igraph, lib.loc = '../packages/')
+library(janitor, lib.loc = '../packages/')
+library(lubridate, lib.loc = '../packages/')
+library(readxl, lib.loc = '../packages/')
 
 # set stan path
 #set_cmdstan_path('/Users/helen/.cmdstanr/cmdstan-8.2')
 
 # set seed
 set.seed(12345)
+
+# make pdf
+pdf('experimenting_with_stan_motnp.pdf')
 
 #### import data frame ####
 counts_df <- read_csv('../data_processed/motnp_binomialpairwiseevents_malesonly.csv')
@@ -22,21 +27,18 @@ edge_binary <- cmdstan_model("models/edge_binary_basic.stan")
 edge_binary
 
 ### create data list -- can contain no NA values in any column, even if column is not specified in model
+num_dyads <- nrow(counts_df)
+num_eles <- length(unique(c(counts_df$id_1, counts_df$id_2)))
 counts_ls <- list(
-  n_dyads = nrow(counts_df),          # total number of times one or other of the dyad was observed
+  n_dyads = num_dyads,                 # total number of times one or other of the dyad was observed
   dyad_ids = counts_df$dyad_males,
   together = counts_df$event_count,    # count number of sightings seen together
-  #apart = counts_df$apart,          # count number of sightings seen apart
-  #seen_together = ifelse(counts_df$event_count > 0, 1, 0),
-  #mu = ifelse(counts_df$event_count > 0, -1.5, -3),
-  #sigma = ifelse(counts_df$event_count > 0, 0.8, 1),
   count_dyad = counts_df$count_dyad
   )
 
 ### Fit model
-num_samples <- 1000
 num_chains <- 4
-num_dyads <- nrow(counts_df)
+num_samples <- 1000
 fit_weight_motnp <- edge_binary$sample(
   data = counts_ls, 
   chains = num_chains, 
@@ -48,6 +50,9 @@ num_iterations <- dim(event_pred)[1]
 
 # Plot the density of the observed event counts
 plot(density(counts_df$event_count), main="", xlab="Dyadic event counts", las=1)
+
+save.image('motnp_edges_fit_experimenting_with_stan.RData')
+#load('motnp_edges_fit_experimenting_with_stan.RData')
 
 # Plot the densities of the predicted event counts, repeat for 10 samples
 df_copy <- counts_df
@@ -64,18 +69,6 @@ logit_edge_samples <- rstan::extract(fit_weight_motnp)$logit_edge # Logit scale 
 edge_samples <- plogis(logit_edge_samples) # (0, 1) scale edge weights
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 # Create edge list
 dyad_name <- do.call(paste, c(counts_df[c("node_1", "node_2")], sep=" <-> "))
 edge_lower <- apply(edge_samples, 2, function(x) quantile(x, probs=0.025))
@@ -89,69 +82,73 @@ edge_list <- cbind(
 rownames(edge_list) <- dyad_name
 edge_list
 
+### check model
+fit_edges_motnp$summary()
+fit_edges_motnp$draws()
+
 # Extract posterior samples
-posterior_samples <- fit_weight_motnp$draws()
+posterior_samples <- fit_edges_motnp$draws()
 
 # Convert the array to a matrix -- save for eigenvector centralities
-total_samples <- n_chains*n_samples
-edge_weights_matrix <- posterior_samples[,,2:(length(posterior_samples)/(total_samples))]
+edge_weights_matrix <- posterior_samples[,,2:(nrow(counts_df)+1)]
 
 # convert matrix to data frame -- save samples and plot outputs
 edges <- as.data.frame(edge_weights_matrix[,,1])
 colnames(edges) <- c('chain1','chain2','chain3','chain4')
 edges <- pivot_longer(edges, everything(), values_to = 'edge_draw', names_to = 'chain')
 edges$dyad <- counts_ls$dyad_ids[1]
-edges$position <- rep(1:n_samples, each = n_chains)
+edges$position <- rep(1:num_samples, each = num_chains)
 for(i in 2:num_dyads){
   x <- as.data.frame(edge_weights_matrix[,,i])
   colnames(x) <- c('chain1','chain2','chain3','chain4')
   x <- pivot_longer(x, everything(), values_to = 'edge_draw', names_to = 'chain')
   x$dyad <- counts_ls$dyad_ids[i]
-  x$position <- rep(1:n_samples, each = n_chains)
+  x$position <- rep(1:num_samples, each = num_chains)
   edges <- rbind(edges, x)
 }
 
 ### save data 
-saveRDS(edges, '../data_processed/motnp_edgedistributions.RDS')
-
-# Plot the densities of the association strengths
-sample_seen <- sample(counts_df$dyad_id[counts_df$event_count > 0], 500, replace = F)
-sample_unsn <- sample(counts_df$dyad_id[counts_df$event_count ==0], 500, replace = F)
-counts_df$sri <- counts_df$event_count / counts_df$count_dyad
+saveRDS(edges, '../data_processed/motnp_edgedistributions_beta1.5.RDS')
+#edges <- readRDS('../data_processed/motnp_edgedistributions_beta1.5.RDS')
 
 
-
-
-
-
-
-
-colnames(edge_samples) <- counts_df$dyad_id
-
-
-
-
-
-
-
-
-
-plot(NULL, main="", xlab="edge weight", ylab="density", las=1, xlim=c(0,1), ylim=c(0,50))
-for (i in 1:length(sample_seen)) {
-  lines(density(edge_samples[,colnames(edge_samples) == sample_seen[i]]), col=rgb(0, 0, 1, 0.05))
-  lines(density(edge_samples[,colnames(edge_samples) == sample_unsn[i]]), col=rgb(1, 0, 0, 0.05))
+#### check outputs -- NETWORK PLOTS USING MY ADAPTED BISONR CODE NOT CURRENTLY WORKING, BUT COME BACK TO THAT ####
+# Assign random set of columns to check
+if(length(which(counts_df$event_count >= 1)) >= 200){ n_test <- 200 } else { n_test <- length(which(counts_df$event_count >= 1)) }
+plot_dyads <- c(sample(counts_df$dyad_males[counts_df$event_count >= 1], size = n_test, replace = F),
+                sample(counts_df$dyad_males[counts_df$event_count == 0], size = n_test, replace = F))
+plot_edges <- edges[edges$dyad %in% plot_dyads,]
+plot_edges$seen_together <- NA
+for(i in 1:length(plot_dyads)){
+  plot_edges$seen_together[plot_edges$dyad == plot_dyads[i]] <- ifelse(counts_df$event_count[counts_df$dyad_males == plot_dyads[i]] > 0, 1, 0)
 }
-lines(density(counts_df$sri[counts_df$event_count > 0]), lwd = 2, col = 'green')
-lines(density(counts_df$sri), lwd = 2)
 
-### check traces
-par(mfrow = c(8,8))
-for (i in 1:32) {
-  plot(edge_samples[,colnames(edge_samples) == sample_seen[i]], type = 'l', col=rgb(0, 0, 1, 0.05))
-  plot(edge_samples[,colnames(edge_samples) == sample_unsn[i]], type = 'l', col=rgb(1, 0, 0, 0.05))
+### build traceplots
+ggplot(data = plot_edges[plot_edges$seen_together == 1,], aes(y = edge_draw, x = position, colour = chain))+
+  geom_line()+
+  facet_wrap(. ~ dyad)+
+  theme_classic()+
+  theme(legend.position = 'none', strip.background = element_blank(), strip.text = element_blank())
+ggplot(data = plot_edges[plot_edges$seen_together == 0,], aes(y = edge_draw, x = position, colour = chain))+
+  geom_line()+
+  facet_wrap(. ~ dyad)+
+  theme_classic()+
+  theme(legend.position = 'none', strip.background = element_blank(), strip.text = element_blank())
+
+### density plots
+plot(NULL, xlim = c(0,1), ylim = c(0,50), las = 1, xlab = 'edge weight', ylab = 'density')
+for(i in 1:length(plot_dyads)){
+  x <- plot_edges[plot_edges$dyad == plot_dyads[i],]
+  lines(density(x$edge_draw), col = ifelse(x$seen_together == 1, rgb(0,0,1,0.1), rgb(1,0,0,0.1)))
 }
-par(mfrow = c(1,1))
 
+### end pdf
+dev.off()
+
+### save image
+save.image('motnp_edgeweights_stanmodel_beta1.5.RData')
+
+### plot network ####
 # Create adjacency array
 eles <- unique(c(counts_df$id_1, counts_df$id_2))
 num_eles <- length(eles)
@@ -189,3 +186,7 @@ coords <- igraph::layout_nicely(g_mid)
 plot(g_mid, edge.width=3 * E(g_mid)$weight, edge.color="black",  layout=coords)
 plot(g_mid, edge.width=20 * E(g_range)$weight, edge.color=rgb(0, 0, 0, 0.25), 
      vertex.label=eles,vertex.label.color="black", layout=coords, add=TRUE)
+
+# save output
+dev.off()
+save.image('experimenting_with_stan_motnp.RData')
