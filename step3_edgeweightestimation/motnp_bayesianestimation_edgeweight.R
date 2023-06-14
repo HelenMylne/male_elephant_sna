efa9a865400feaced097594282354d2903804e5d
HelenMylne <hkm513@york.ac.uk>
2023-06-14 11:56:52 +0100

    cleaning, reduce node size in plots, correct lines copied from ANP1

--- step3_edgeweightestimation/motnp_bayesianestimation_edgeweight.R
+++ step3_edgeweightestimation/motnp_bayesianestimation_edgeweight.R
@@ -6,7 +6,7 @@
 # Data input: raw data provided by ALERT processed using script 22.01.13_ALERT_bayesian.R
 
 #### set up ####
-# install packages
+### install packages
 # install.packages('tidyverse')
 # install.packages('dplyr')
 # install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
@@ -20,75 +20,75 @@
 # install.packages('hms')
 # install.packages('readxl')
 
-# load packages
+### load packages
 # library(tidyverse) ; library(dplyr) ; library(cmdstanr) ; library(dagitty) ; library(igraph) ; library(janitor) ; library(lubridate) ; library(hms) ; library(readxl)
 # set_cmdstan_path('../packages/.cmdstan/cmdstan-2.31.0/')
-library(cmdstanr, lib.loc = 'packages/')    # library(cmdstanr)
-library(tidyverse, lib.loc = 'packages/')   # library(tidyverse)
-library(dplyr, lib.loc = 'packages/')       # library(dplyr)
-#library(rstan, lib.loc = 'packages/')      # library(rstan)
-library(dagitty)
-library(igraph, lib.loc = 'packages/')      # library(igraph)
-library(janitor, lib.loc = 'packages/')     # library(janitor)
-library(lubridate, lib.loc = 'packages/')   # library(lubridate)
-library(readxl, lib.loc = 'packages/')      # library(readxl)
+library(cmdstanr, lib.loc = '../packages/')    # library(cmdstanr)
+library(tidyverse, lib.loc = '../packages/')   # library(tidyverse)
+library(dplyr, lib.loc = '../packages/')       # library(dplyr)
+#library(rstan, lib.loc = '../packages/')      # library(rstan)
+#library(dagitty)
+library(igraph, lib.loc = '../packages/')      # library(igraph)
+library(janitor, lib.loc = '../packages/')     # library(janitor)
+library(lubridate, lib.loc = '../packages/')   # library(lubridate)
+library(readxl, lib.loc = '../packages/')      # library(readxl)
 
-# set stan path
+### set stan path
 #set_cmdstan_path('/Users/helen/.cmdstan/cmdstan-2.32.1')
 #set_cmdstan_path('../packages/.cmdstan/cmdstan-2.31.0')
 
-# set seed
+### set seed
 set.seed(12345)
 
-# create file of output graphs
+### create file of output graphs
 pdf('../outputs/motnp_edgeweights_conditionalprior.pdf', width = 20, height = 20)
 
 #### draw dyadic regression DAGS ####
-# plot with full names
-#binom <- dagitty::dagitty("dag{
-#                         age_1 [exposure];
-#                         age_2 [exposure];
-#                         edgeweight [outcome];
-#                         relatedness_dyad [unobserved];
-#                         age_1 -> edgeweight <- age_2;
-#                         edgeweight <- relatedness_dyad;
-#                         }")
-#dagitty::coordinates(binom) <- list(x = c(age_1 = 0, age_2 = 2, edgeweight = 1, relatedness_dyad = 1),
-#                                    y = c(age_1 = 0, age_2 = 0, edgeweight = 1, relatedness_dyad = 2))
-#rethinking::drawdag(binom)
+### plot with full names
+binom <- dagitty::dagitty("dag{
+                         age_1 [exposure];
+                         age_2 [exposure];
+                         edgeweight [outcome];
+                         relatedness_dyad [unobserved];
+                         age_1 -> edgeweight <- age_2;
+                         edgeweight <- relatedness_dyad;
+                         }")
+dagitty::coordinates(binom) <- list(x = c(age_1 = 0, age_2 = 2, edgeweight = 1, relatedness_dyad = 1),
+                                    y = c(age_1 = 0, age_2 = 0, edgeweight = 1, relatedness_dyad = 2))
+rethinking::drawdag(binom)
 
-# plot with letters
-#binom <- dagitty::dagitty("dag{
-#                         A_1 [exposure];
-#                         A_2 [exposure];
-#                         EW [outcome];
-#                         R_d [unobserved];
-#                         A_1 -> EW <- A_2;
-#                         EW <- R_d;
-#                         }")
-#dagitty::coordinates(binom) <- list(x = c(A_1 = 0, A_2 = 2, EW = 1, R_d = 1),
-#                                    y = c(A_1 = 0, A_2 = 0, EW = 1, R_d = 2))
-#rethinking::drawdag(binom, radius = 6, cex = 1.6)
+### plot with letters
+binom <- dagitty::dagitty("dag{
+                         A_1 [exposure];
+                         A_2 [exposure];
+                         EW [outcome];
+                         R_d [unobserved];
+                         A_1 -> EW <- A_2;
+                         EW <- R_d;
+                         }")
+dagitty::coordinates(binom) <- list(x = c(A_1 = 0, A_2 = 2, EW = 1, R_d = 1),
+                                    y = c(A_1 = 0, A_2 = 0, EW = 1, R_d = 2))
+rethinking::drawdag(binom, radius = 6, cex = 1.6)
 
-# clear environment and reset plot window
-#rm(binom)
+### clear environment and reset plot window
+rm(binom)
 
 #### complete data processing ####
-# load in age data
+### load in age data
 motnp_ages <- readRDS('../data_processed/motnp_ageestimates_mcmcoutput.rds') %>%
   pivot_longer(cols = everything(), names_to = 'id', values_to = 'age')
 
-# import data for aggregated model (binomial) -- counts of positive associations and total sightings
+### import data for aggregated model (binomial) -- counts of positive associations and total sightings
 counts_df <- read_csv('../data_processed/motnp_bayesian_binomialpairwiseevents.csv')
 
-# correct sex_1, which has loaded in as a logical vector not a character/factor
+### correct sex_1, which has loaded in as a logical vector not a character/factor
 unique(counts_df$sex_1) # FALSE or NA
 sex_1 <- data.frame(sex_1 = counts_df$id_1)
 sex_1 <- sex_1 %>% separate(sex_1, c("sex", "number"), sep = 1, remove = FALSE) ; unique(sex_1$sex) # F, M, U
 counts_df$sex_1 <- as.character(sex_1$sex) ; rm(sex_1) ; gc()
 str(counts_df)  # sex_1 still comes up as logical, but now contains the right levels
 
-# create variable for age difference
+### create variable for age difference
 unique(counts_df$age_category_1) # "0-3","1-2","3-4","4-5","5-6","6-7","7-8","8-9","9-10","10-15","15-19","20-25","20-35","25-40","35-50","40+","50+",NA --> convert anything <5 to cat 1, and anything 5-10 to cat 2
 counts_df$age_cat_id_1 <- ifelse(counts_df$age_category_1 == '0-3', 1,
                                  ifelse(counts_df$age_category_1 == '3-4', 1,
@@ -135,7 +135,7 @@ counts_df$age_class_2 <- ifelse(counts_df$age_cat_id_2 == 1, 'Calf',
                                 ifelse(counts_df$age_cat_id_2 == 2, 'Juvenile',
                                        ifelse(counts_df$age_cat_id_2 > 4, 'Adult','Pubescent')))
 
-# correct dem_class with corrected age classes
+### correct dem_class with corrected age classes
 counts_df$dem_class_1 <- ifelse(counts_df$age_class_1 == 'Adult', paste('A',counts_df$sex_1, sep = ''),
                                 ifelse(counts_df$age_class_1 == 'Pubescent', paste('P',counts_df$sex_1, sep = ''),
                                        ifelse(counts_df$age_class_1 == 'Juvenile', paste('J',counts_df$sex_1, sep = ''),
@@ -145,7 +145,7 @@ counts_df$dem_class_2 <- ifelse(counts_df$age_class_2 == 'Adult', paste('A',coun
                                        ifelse(counts_df$age_class_2 == 'Juvenile', paste('J',counts_df$sex_2, sep = ''),
                                               paste('C',counts_df$sex_2, sep = ''))))
 
-# combined dem_class of dyad
+### combined dem_class of dyad
 counts_df$age_class_id_1 <- ifelse(counts_df$age_class_1 == 'Adult',4,
                                    ifelse(counts_df$age_class_1 == 'Pubescent',3,
                                           ifelse(counts_df$age_class_1 == 'Juvenile',2,1)))
@@ -165,16 +165,16 @@ counts_df$dem_type <- ifelse(counts_df$age_class_id_1 > counts_df$age_class_id_2
                                                          paste(counts_df$dem_class_2, counts_df$dem_class_1, sep = '_')))))) # ...when 2 is M or both are U: dc2_dc1
 sort(unique(counts_df$dem_type))
 
-# add column for age difference between dyad
+### add column for age difference between dyad
 counts_df$age_diff <- abs(as.numeric(counts_df$age_cat_id_1) - as.numeric(counts_df$age_cat_id_2))
 
-# add column for total number of sightings per pair
+### add column for total number of sightings per pair
 counts_df$count_dyad <- (counts_df$count_1 + counts_df$count_2) - counts_df$event_count  # maximum possible sightings of pairing = sum of times see node_1 and times see node_2, but this includes the times they were seen together twice, so then subtract once the count of paired sightings.
 
-# add column for total number of sightings per pair where they were NOT together
+### add column for total number of sightings per pair where they were NOT together
 counts_df$apart <- counts_df$count_dyad - counts_df$event_count
 
-# reassign dyad numbers to remove gaps
+### reassign dyad numbers to remove gaps
 counts_df$node_1_nogaps <- as.integer(as.factor(counts_df$node_1))
 counts_df$node_2_nogaps <- as.integer(as.factor(counts_df$node_2))+1
 
@@ -208,10 +208,10 @@ write_csv(counts_df, '../data_processed/motnp_binomialpairwiseevents_malesonly.c
 ### add time marker
 print(paste0('data read in at ', Sys.time()))
 
-#### create data list ####
-# create nodes data frame
+##### create data list ####
+### create nodes data frame
 nodes <- data.frame(id = sort(unique(c(counts_df$id_1,counts_df$id_2))),  # all unique individuals
-                    node = NA, age = NA, sightings = NA)                  # data needed on each
+                   node = NA, age = NA, sightings = NA)                  # data needed on each
 for(i in 1:nrow(nodes)){
   # extract data about individual from counts_df data frame
   if(nodes$id[i] %in% counts_df$id_1) {
@@ -235,7 +235,7 @@ counts_ls <- list(
   n_dyads    = n_dyads,                      # total number of times one or other of the dyad was observed
   dyad_ids   = counts_df$dyad_id,            # identifier for each dyad
   together   = counts_df$event_count,        # count number of sightings seen together
-  count_dyad = counts_df$count_dyad,         # count total number of times seen
+  count_dyad = counts_df$count_dyad          # count total number of times seen
 )
 
 #### compile Stan model ####
@@ -276,8 +276,8 @@ for(i in 2:n_dyads){
 saveRDS(edges, '../data_processed/motnp_edgedistributions_conditionalprior.RDS')
 #edges <- readRDS('../data_processed/motnp_edgedistributions_conditionalprior.RDS')
 
-#### check outputs: edge weights ####
-# Assign random set of columns to check
+##### check outputs: edge weights ####
+### Assign random set of columns to check
 if(length(which(counts_df$event_count >= 1)) >= 200){ n_test <- 200 } else { n_test <- length(which(counts_df$event_count >= 1)) }
 plot_dyads <- c(sample(counts_df$dyad_id[counts_df$event_count >= 1], size = n_test, replace = F),
                 sample(counts_df$dyad_id[counts_df$event_count == 0], size = n_test, replace = F))
@@ -298,7 +298,7 @@ ggplot(data = plot_edges[plot_edges$seen_together == 0,], aes(y = edge_draw, x =
   theme_classic()+
   theme(legend.position = 'none', strip.background = element_blank(), strip.text = element_blank())
 
-### density plots
+#### density plots
 plot(NULL, xlim = c(0,1), ylim = c(0,30), las = 1, xlab = 'edge weight', ylab = 'density')
 for(i in 1:length(plot_dyads)){
   x <- plot_edges[plot_edges$dyad == plot_dyads[i],]
@@ -374,7 +374,7 @@ plot_network_threshold <- function (edge_samples, dyad_data, lwd = 2, threshold 
                                             ifelse(node_age < 20, '#55C667FF',
                                                    ifelse(node_age < 30, '#1F968BFF', 
                                                           ifelse(node_age < 40, '#39568CFF', '#440154FF')))), 
-                      vertex.size = node_sightings,
+                      vertex.size = log(node_sightings)*5,
                       frame.color = NA, frame.width = 0,
                       edge.color = NA, edge.arrow.size = 0, edge.width = 0)
   igraph::plot.igraph(net, layout = coords, add = TRUE,
@@ -416,9 +416,6 @@ save.image('motnp_edgeweights_conditionalpriors.RData')
 #load('motnp_edgeweights_conditionalpriors.RData')
 
 #### check outputs: compare to simple SRI ####
-### load in workspace image with edge weights already calculated
-#load('motnp_edgeweights_conditionalpriors.RData')
-
 ### extract edgelist and calculate SRI
 make_edgelist <- function (edge_samples, dyad_data)
 {
@@ -438,16 +435,13 @@ make_edgelist <- function (edge_samples, dyad_data)
   edgelist <- edgelist[,c(4:5,1:3)]
   return(edgelist)
 }
-edgelist <- make_edgelist(edge_samples = edge_samples, dyad_data = cdf_1)     # obtain edge list
+edgelist <- make_edgelist(edge_samples = edge_samples, dyad_data = counts_df)       # obtain edge list
 head(edgelist)                                                                      # check structure of edgelist
-colnames(edgelist)[1:2] <- colnames(counts_df_model)[1:2]                           # rename for join function
-edgelist$node_1_id <- as.integer(edgelist$node_1_id)                                # convert to integers
-edgelist$node_2_id <- as.integer(edgelist$node_2_id)                                # convert to integers
-summary <- left_join(edgelist, counts_df_model, by = c('node_1_id','node_2_id'))    # combine distribution data with raw counts
-counts <- counts_df[,c('node_1_males','node_2_males','count_1','count_2')]          # add individual count data
-colnames(counts)[1:2] <- c('node_1_id','node_2_id')                                 # rename for join function
-summary <- left_join(summary, counts, by = c('node_1_id','node_2_id'))              # combine summary data with individual counts
-summary$sri <- summary$event / (summary$duration)                                   # calculate basic SRI
+colnames(edgelist)[1:2] <- c('node_1','node_2')                                     # rename for join function
+edgelist$node_1 <- as.integer(edgelist$node_1)                                      # convert to integers
+edgelist$node_2 <- as.integer(edgelist$node_2)                                      # convert to integers
+summary <- left_join(edgelist, counts_df, by = c('node_1','node_2'))              # combine distribution data with raw counts
+summary$sri <- summary$event_count / summary$count_dyad                             # calculate basic SRI
 
 ### plot median vs SRI
 plot(density(summary$sri), main = 'red = SRI, blue = BISoN', col = 'red', lwd = 2, las = 1) # plot SRI distribution
@@ -459,11 +453,10 @@ lines(density(summary$median[which(summary$count_1 >= 12 & summary$count_2 >= 12
 
 # try plotting a subset with facets showing the draw distributions and lines indicating the position of standard SRI calculation
 plot_edges$dyad_id <- as.numeric(plot_edges$dyad)
-draws <- left_join(plot_edges, summary, by = 'dyad_id') %>%                  # combine to add node and median value data
-  select(-dyad.x) %>% rename(dyad = dyad.y)                                  # clean up names
+draws <- left_join(plot_edges, summary, by = 'dyad_id')                      # combine to add node and median value data
 which(is.na(draws) == TRUE)                                                  # check fully merged
 
-draws$dyad_id <- reorder(draws$dyad_id, draws$period_count_dyad)             # order data frame based on total sightings per pair
+draws$dyad_id <- reorder(draws$dyad_id, draws$count_dyad)                    # order data frame based on total sightings per pair
 ggplot(data = draws, mapping = aes(x = edge_draw)) +                         # plot data frame
   geom_density(colour = 'blue') +                                            # plot density plots of probability distributions output per dyad
   facet_wrap(. ~ dyad_id, nrow = 20) +                                       # split by dyad, allow to vary height depending on dyad
