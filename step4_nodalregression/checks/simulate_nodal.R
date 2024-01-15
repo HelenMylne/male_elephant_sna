21d8ea84810b217c2e636bc29a49447500401747
HelenMylne <hkm513@york.ac.uk>
2024-01-15 12:44:28 +0000

    merge versions on 2 separate computers

--- step4_nodalregression/checks/simulate_nodal.R
+++ step4_nodalregression/checks/simulate_nodal.R
@@ -3,6 +3,8 @@ library(LaplacesDemon)
 library(tidyverse)
 library(cmdstanr)
 
+#set_cmdstan_path('../packages/.cmdstan/cmdstan-2.31.0/')
+
 ## simulate population ####
 ## define population parameters
 min_age <- 11                                                # youngest individual
@@ -128,7 +130,7 @@ plot(NULL, las = 1, xlab = 'age (standardised)', ylab = 'eigenvector (standardis
 abline(h = min(sim_cent_mu), lty = 2) ; abline(h = max(sim_cent_mu), lty = 2)
 for(i in 1:n){
   lines(x = seq(min(sim$age_std), max(sim$age_std), length.out = 2),
-        y = intercept[i] + beta_age[i]*seq(min(sim$age_std), max(sim$age_std), length.out = 2),
+        y = intercept[i] + beta_age[i]*c(min(sim$age_std), max(sim$age_std)),
         col = rgb(0,0,1,0.4))
 } # looks mad when I set true slope very small, but decent when larger -- given that I have no idea what the real version should be, I've kept them relatively weak
 
@@ -231,178 +233,225 @@ sim$mean_predict_invlogit <- invlogit(sim$predict_ustd)
 sim$lwr_invlogit <- invlogit(sim$lwr_ustd)
 sim$upr_invlogit <- invlogit(sim$upr_ustd)
 sim$mu_invlogit <- invlogit(sim$mu)
+<<<<<<< Updated upstream
 ggplot(sim)+
   geom_ribbon(aes(x = age, ymin = lwr_invlogit, ymax = upr_invlogit),
               fill = rgb(1,1,0,0.5))+
   geom_point(aes(y = mu_invlogit, x = age))+
   geom_line(aes(y = mean_predict_invlogit, x = age))+
   theme_bw()
+=======
+sim_sort <- sim[order(sim$age),]
+plot(sim_sort$mu_invlogit ~ sim_sort$age, ylim = c(0,1))
+lines(sim_sort$mean_predict_invlogit ~ sim_sort$age)
+polygon(y = c(sim_sort$lwr_invlogit, rev(sim_sort$upr_invlogit)),
+        x = c(sim_sort$age, rev(sim_sort$age)),
+        col = rgb(1,1,0,0.5), border = NA)
+>>>>>>> Stashed changes
 
-## run model -- age as an ordered categorical variable with 1 window: run all above as is but change definition of sim1 to sim and then don't rbind to sim2 ####
-## categorise age
-sim$age_cat <- ifelse(sim$age <= 15, 1,
-                      ifelse(sim$age <= 20, 2,
-                             ifelse(sim$age <= 25, 3,
-                                    ifelse(sim$age <= 40, 4, 5))))
+## full predictions
+ages <- seq(min(sim$age_std), max(sim$age_std), length.out = 10)
+pred_data <- data.frame(age_std = rep(ages, each = n_nodes*n_windows),
+                        node = rep(rep(1:n_nodes, each = n_windows), length(ages)),
+                        window = rep(1:2, n_nodes*length(ages)),
+                        prediction = NA)
+predictions <- pred_data
+mu <- params$beta_age[1]*predictions$age_std + params$intercept[1] + as.numeric(rand_window[1,predictions$window]) + as.numeric(rand_node[1,predictions$node])
+#sigma <- sim_cent_cov + diag(rep(params$sigma[i], n_data))
+predictions$prediction <- mu #MASS::mvrnorm(1, mu, sigma) # should be using the full distribution not just the mean to do this, but then I have to use the raw data for predictions because otherwise sim_cent_cov is the wrong dimensions in defining Sigma
 
-## create data
-n_age_cat <- length(unique(sim$age_cat))
-eigen_list <- list(#num_data = n_data,
-  num_nodes = n_nodes,
-  num_age_cat = n_age_cat,
-  length_dirichlet = n_age_cat + 1,
-  #num_windows = length(unique(sim$window)),
-  centrality_mu = sim_cent_mu,
-  centrality_cov = sim_cent_cov,
-  node_age = sim$age_cat,
-  #window = sim$window,
-  #node = sim$node,
-  prior_age = rep(1, n_age_cat))
-
-## check inputs
-plot(sim_cent_mu ~ sim$age_cat)
-
-## prior predictive check
-n <- 100
-beta_age <- rnorm(n, 0, 1)
-intercept  <- rnorm(n, 0, 0.8)
-age_dirichlet <- rdirichlet(n, c(1,1,1,1,1))
-plot(NULL, las = 1, xlab = 'age (standardised)', ylab = 'eigenvector (standardised)',
-     ylim = c(min(sim_cent_mu)-1, max(sim_cent_mu)+1), xlim = c(min(sim$age_cat), max(sim$age_cat)))
-abline(h = min(sim_cent_mu), lty = 2) ; abline(h = max(sim_cent_mu), lty = 2)
-x <- min(sim$age_cat):max(sim$age_cat)
-for(i in 1:n){
-  y <- rep(NA, length(x))
-  for(j in 1:length(x)){
-    y[j] <- intercept[i] + beta_age[i]*sum(age_dirichlet[i,][1:x[j]])
-  }
-  lines(x = x, y = y, col = rgb(0,0,1,0.4))
+for(i in 2:nrow(params)) {
+  mu <- params$beta_age[i]*pred_data$age_std + params$intercept[i] + as.numeric(rand_window[i,pred_data$window]) + as.numeric(rand_node[i,pred_data$node])
+  sigma <- sim_cent_cov + diag(rep(params$sigma[i], n_data))
+  pred_data$prediction <- mu#MASS::mvrnorm(1, mu, sigma)
+  predictions <- rbind(predictions, pred_data)
+  if(i %% 100 == 0) { print(i) }
 }
-rm(n, beta_age, intercept, age_dirichlet, sigma, x, y, df_plot, df_wide) ; gc()
+rm(mu, sigma, pred_data, i,j,k) ; gc()
 
-## load model
-nodal_regression <- cmdstan_model('models/eigen_regression_motnp.stan')
-
-## run model
-n_chains <- 4
-n_samples <- 1000
-#fit_sim <- sampling(nodal_regression, data = eigen_list, chains = n_chains, cores = n_chains)
-fit_sim <- nodal_regression$sample(data = eigen_list,
-                                   chains = n_chains, parallel_chains = n_chains,
-                                   iter_warmup = n_samples, iter_sampling = n_samples)
-
-## check outputs ####
-## view summary
-fit_sim$summary()
-
-## extract posterior
-#params <- rstan::extract(fit_sim)
-params <- fit_sim$draws(format = 'draws_df')
-#rand_window <- params %>% 
-#  dplyr::select(`rand_window[1]`,`rand_window[2]`)
-#rand_node <- params[,16:(n_nodes+2)]
-delta <- params %>% 
-  select(`delta[1]`,`delta[2]`,`delta[3]`,`delta[4]`,`delta[5]`)
-delta_j <- params %>% 
-  select(`delta_j[1]`,`delta_j[2]`,`delta_j[3]`,`delta_j[4]`,`delta_j[5]`,`delta_j[6]`)
-
-## traceplot linear effect size
-#traceplot(fit_sim, pars = c('intercept','beta_age','sigma','predictor[1]','predictor[50]','predictor[100]'))
-parameters_to_check <- c('intercept','beta_age','sigma','predictor[1]','predictor[20]','predictor[50]')#, 'rand_window[1]','rand_window[2]','rand_node[1]','rand_node[50]','rand_node[100]')
-params %>% 
-  select(all_of(parameters_to_check)) %>%
-  pivot_longer(cols = everything(), names_to = 'parameter', values_to = 'draw') %>% 
-  mutate(chain_position = rep(rep(1:n_samples, each = length(parameters_to_check)), n_chains),
-         chain = rep(1:n_chains, each = length(parameters_to_check)*n_samples)) %>% 
-  #filter(chain == 4) %>% 
-  ggplot(aes(x = chain_position, y = draw, colour = as.factor(chain)))+
-  geom_line()+
-  facet_wrap(. ~ parameter, scales = 'free_y')+
-  theme_bw()+
+## plot predictions
+pred_data <- predictions %>% 
+  group_by(age_std, node, window) %>% 
+  mutate(mean = mean(prediction)) %>% 
+  ungroup() %>% 
+  select(age_std, node, window, mean) %>% 
+  distinct() %>% 
+  left_join(distinct(sim[,c('age','age_std')]), by = 'age_std')
+ggplot()+
+  geom_point(data = sim,
+             mapping = aes(x = age_std, y = mu_std,
+                           colour = as.factor(node)))+
+  geom_line(data = pred_data,
+            mapping = aes(x = age_std, y = mean))+
+  facet_wrap(. ~ window)+
+  theme_classic()+
   theme(legend.position = 'none')
 
-## posterior predictive check
-plot(density(sim_dat_std[1, ]), las = 1, ylim = c(0,0.4),
-     main = "Posterior predictive check (standardised centrality):\nblack = data, blue = predicted",
-     col=rgb(0, 0, 0, 0.25))
-for (i in 1:100) {
-  j <- sample(1:length(params$beta_age), 1)
-  lines(density(sim_dat_std[j, ]), col=rgb(0, 0, 0, 0.25))
-  mu <- rep(NA, length(eigen_list$node_age))
-  for(k in 1:length(eigen_list$node_age)){
-    mu[k] <- params$intercept[j] + params$beta_age[j]*sum(delta_j[j,(1:eigen_list$node_age[k])]) #+ as.numeric(rand_window[j,eigen_list$window[k]]) + as.numeric(rand_node[j,eigen_list$nodes[k]])
-  }
-  sigma <- sim_cent_cov + diag(rep(params$sigma[j], n_data))
-  lines(density(MASS::mvrnorm(1, mu, sigma)), col=rgb(0, 0, 1, 0.25))
-}
-
-## predict from model
-new_data <- data.frame(node = 1:length(unique(sim$age_cat)),
-                       age_cat = rep(sort(unique(sim$age_cat))))
-predict <- matrix(data = NA, nrow = length(params$beta_age), ncol = nrow(new_data),
-                  dimnames = list(1:length(params$beta_age),
-                                  1:length(unique(sim$age_cat))))
-for(i in 1:nrow(predict)){
-  for(j in 1:ncol(predict)){
-    predict[i,j] <- MASS::mvnorm(n = 1,
-                                 mu = params$intercept[i] + params$beta_age[i] * sum(delta_j[i,(1:j)]),
-                                 Sigma = params$sigma[i])
-    #predict[i,j] <- params$intercept[i] + params$beta_age[i] * sum(delta_j[i,(1:j)])
-  }
-}
-new_data$mean_predict <- apply(predict, 2, mean)
-new_data$lwr_predict <- apply(predict, 2, quantile, probs = 0.025)
-new_data$upr_predict <- apply(predict, 2, quantile, probs = 0.975)
-
-## compare to standardised raw data
-plot(new_data$mean_predict ~ new_data$age_cat, las = 1, type = 'l', ylim = c(-2,2))        # plot against age
-points(sim$mu_std ~ sim$age_cat)                                                           # add raw points
-polygon(y = c(new_data$lwr_predict, rev(new_data$upr_predict)), x = c(new_data$age_cat, rev(new_data$age_cat)),
-        col = rgb(1,1,0,0.5), border = NA)
-
-## unstandardise predictions
-sim$mu
-(new_data$predict_ustd <- new_data$mean_predict * sd(sim$mu) + mean(sim$mu))
-sim <- sim %>% 
-  left_join(new_data, by = 'age_cat')
-plot(sim$predict_ustd ~ sim$mu)
-abline(a = 0, b = 1)
-
-## compare to unstandardised raw data
-sim$lwr_ustd <- sim$lwr_predict * sd(sim$mu) + mean(sim$mu)
-sim$upr_ustd <- sim$upr_predict * sd(sim$mu) + mean(sim$mu)
-plot(sim$predict_ustd ~ sim$age_cat, las = 1, type = 'l')        # plot against age
-points(sim$mu ~ sim$age_cat)                                     # add raw points
-polygon(y = c(sim$lwr_ustd, rev(sim$upr_ustd)), x = c(sim$age_cat, rev(sim$age_cat)),
-        col = rgb(1,1,0,0.5), border = NA)
-
-## convert to invlogit scale
-sim$mean_predict_invlogit <- invlogit(sim$predict_ustd)
-sim$lwr_invlogit <- invlogit(sim$lwr_ustd)
-sim$upr_invlogit <- invlogit(sim$upr_ustd)
-sim$mu_invlogit <- invlogit(sim$mu)
-
-## compare to invlogit raw data
-plot(sim$mu_invlogit ~ sim$age_cat, ylim = c(0,1))
-lines(sim$mean_predict_invlogit ~ sim$age_cat)
-polygon(y = c(sim$lwr_invlogit, rev(sim$upr_invlogit)), x = c(sim$age_cat, rev(sim$age_cat)),
-        col = rgb(1,1,0,0.5), border = NA)
-
-## extract slope estimates -- none of these match the original input, but the predictions work??!
-# original (true) slope value
-sim_slope
-# predicted (modelled) slope value = mean of differences bewteen categories, divided by the number of years each category represents
-mean( c( (new_data$predict_ustd[2] - new_data$predict_ustd[1]) / (16-10), 
-         (new_data$predict_ustd[3] - new_data$predict_ustd[2]) / (21-16), 
-         (new_data$predict_ustd[4] - new_data$predict_ustd[3]) / (26-21),
-         (new_data$predict_ustd[5] - new_data$predict_ustd[4]) / (40-26) )
-)
-
-## extract model fit
-summary <- fit_sim$summary()
-par(mfrow = c(3,1))
-hist(summary$rhat, breaks = 50)
-hist(summary$ess_bulk, breaks = 50)
-hist(summary$ess_tail, breaks = 50)
-par(mfrow = c(1,1))
-
+# ## run model -- age as an ordered categorical variable with 1 window: run all above as is but change definition of sim1 to sim and then don't rbind to sim2 ####
+# ## categorise age
+# sim$age_cat <- ifelse(sim$age <= 15, 1,
+#                       ifelse(sim$age <= 20, 2,
+#                              ifelse(sim$age <= 25, 3,
+#                                     ifelse(sim$age <= 40, 4, 5))))
+# 
+# ## create data
+# n_age_cat <- length(unique(sim$age_cat))
+# eigen_list <- list(#num_data = n_data,
+#   num_nodes = n_nodes,
+#   num_age_cat = n_age_cat,
+#   length_dirichlet = n_age_cat + 1,
+#   #num_windows = length(unique(sim$window)),
+#   centrality_mu = sim_cent_mu,
+#   centrality_cov = sim_cent_cov,
+#   node_age = sim$age_cat,
+#   #window = sim$window,
+#   #node = sim$node,
+#   prior_age = rep(1, n_age_cat))
+# 
+# ## check inputs
+# plot(sim_cent_mu ~ sim$age_cat)
+# 
+# ## prior predictive check
+# n <- 100
+# beta_age <- rnorm(n, 0, 1)
+# intercept  <- rnorm(n, 0, 0.8)
+# age_dirichlet <- rdirichlet(n, c(1,1,1,1,1))
+# plot(NULL, las = 1, xlab = 'age (standardised)', ylab = 'eigenvector (standardised)',
+#      ylim = c(min(sim_cent_mu)-1, max(sim_cent_mu)+1), xlim = c(min(sim$age_cat), max(sim$age_cat)))
+# abline(h = min(sim_cent_mu), lty = 2) ; abline(h = max(sim_cent_mu), lty = 2)
+# x <- min(sim$age_cat):max(sim$age_cat)
+# for(i in 1:n){
+#   y <- rep(NA, length(x))
+#   for(j in 1:length(x)){
+#     y[j] <- intercept[i] + beta_age[i]*sum(age_dirichlet[i,][1:x[j]])
+#   }
+#   lines(x = x, y = y, col = rgb(0,0,1,0.4))
+# }
+# rm(n, beta_age, intercept, age_dirichlet, sigma, x, y, df_plot, df_wide) ; gc()
+# 
+# ## load model
+# nodal_regression <- cmdstan_model('models/eigen_regression_motnp.stan')
+# 
+# ## run model
+# n_chains <- 4
+# n_samples <- 1000
+# #fit_sim <- sampling(nodal_regression, data = eigen_list, chains = n_chains, cores = n_chains)
+# fit_sim <- nodal_regression$sample(data = eigen_list,
+#                                    chains = n_chains, parallel_chains = n_chains,
+#                                    iter_warmup = n_samples, iter_sampling = n_samples)
+# 
+# ## check outputs ####
+# ## view summary
+# fit_sim$summary()
+# 
+# ## extract posterior
+# #params <- rstan::extract(fit_sim)
+# params <- fit_sim$draws(format = 'draws_df')
+# #rand_window <- params %>% 
+# #  dplyr::select(`rand_window[1]`,`rand_window[2]`)
+# #rand_node <- params[,16:(n_nodes+2)]
+# delta <- params %>% 
+#   select(`delta[1]`,`delta[2]`,`delta[3]`,`delta[4]`,`delta[5]`)
+# delta_j <- params %>% 
+#   select(`delta_j[1]`,`delta_j[2]`,`delta_j[3]`,`delta_j[4]`,`delta_j[5]`,`delta_j[6]`)
+# 
+# ## traceplot linear effect size
+# #traceplot(fit_sim, pars = c('intercept','beta_age','sigma','predictor[1]','predictor[50]','predictor[100]'))
+# parameters_to_check <- c('intercept','beta_age','sigma','predictor[1]','predictor[20]','predictor[50]')#, 'rand_window[1]','rand_window[2]','rand_node[1]','rand_node[50]','rand_node[100]')
+# params %>% 
+#   select(all_of(parameters_to_check)) %>%
+#   pivot_longer(cols = everything(), names_to = 'parameter', values_to = 'draw') %>% 
+#   mutate(chain_position = rep(rep(1:n_samples, each = length(parameters_to_check)), n_chains),
+#          chain = rep(1:n_chains, each = length(parameters_to_check)*n_samples)) %>% 
+#   #filter(chain == 4) %>% 
+#   ggplot(aes(x = chain_position, y = draw, colour = as.factor(chain)))+
+#   geom_line()+
+#   facet_wrap(. ~ parameter, scales = 'free_y')+
+#   theme_bw()+
+#   theme(legend.position = 'none')
+# 
+# ## posterior predictive check
+# plot(density(sim_dat_std[1, ]), las = 1, ylim = c(0,0.4),
+#      main = "Posterior predictive check (standardised centrality):\nblack = data, blue = predicted",
+#      col=rgb(0, 0, 0, 0.25))
+# for (i in 1:100) {
+#   j <- sample(1:length(params$beta_age), 1)
+#   lines(density(sim_dat_std[j, ]), col=rgb(0, 0, 0, 0.25))
+#   mu <- rep(NA, length(eigen_list$node_age))
+#   for(k in 1:length(eigen_list$node_age)){
+#     mu[k] <- params$intercept[j] + params$beta_age[j]*sum(delta_j[j,(1:eigen_list$node_age[k])]) #+ as.numeric(rand_window[j,eigen_list$window[k]]) + as.numeric(rand_node[j,eigen_list$nodes[k]])
+#   }
+#   sigma <- sim_cent_cov + diag(rep(params$sigma[j], n_data))
+#   lines(density(MASS::mvrnorm(1, mu, sigma)), col=rgb(0, 0, 1, 0.25))
+# }
+# 
+# ## predict from model
+# new_data <- data.frame(node = 1:length(unique(sim$age_cat)),
+#                        age_cat = rep(sort(unique(sim$age_cat))))
+# predict <- matrix(data = NA, nrow = length(params$beta_age), ncol = nrow(new_data),
+#                   dimnames = list(1:length(params$beta_age),
+#                                   1:length(unique(sim$age_cat))))
+# for(i in 1:nrow(predict)){
+#   for(j in 1:ncol(predict)){
+#     predict[i,j] <- MASS::mvnorm(n = 1,
+#                                  mu = params$intercept[i] + params$beta_age[i] * sum(delta_j[i,(1:j)]),
+#                                  Sigma = params$sigma[i])
+#     #predict[i,j] <- params$intercept[i] + params$beta_age[i] * sum(delta_j[i,(1:j)])
+#   }
+# }
+# new_data$mean_predict <- apply(predict, 2, mean)
+# new_data$lwr_predict <- apply(predict, 2, quantile, probs = 0.025)
+# new_data$upr_predict <- apply(predict, 2, quantile, probs = 0.975)
+# 
+# ## compare to standardised raw data
+# plot(new_data$mean_predict ~ new_data$age_cat, las = 1, type = 'l', ylim = c(-2,2))        # plot against age
+# points(sim$mu_std ~ sim$age_cat)                                                           # add raw points
+# polygon(y = c(new_data$lwr_predict, rev(new_data$upr_predict)), x = c(new_data$age_cat, rev(new_data$age_cat)),
+#         col = rgb(1,1,0,0.5), border = NA)
+# 
+# ## unstandardise predictions
+# sim$mu
+# (new_data$predict_ustd <- new_data$mean_predict * sd(sim$mu) + mean(sim$mu))
+# sim <- sim %>% 
+#   left_join(new_data, by = 'age_cat')
+# plot(sim$predict_ustd ~ sim$mu)
+# abline(a = 0, b = 1)
+# 
+# ## compare to unstandardised raw data
+# sim$lwr_ustd <- sim$lwr_predict * sd(sim$mu) + mean(sim$mu)
+# sim$upr_ustd <- sim$upr_predict * sd(sim$mu) + mean(sim$mu)
+# plot(sim$predict_ustd ~ sim$age_cat, las = 1, type = 'l')        # plot against age
+# points(sim$mu ~ sim$age_cat)                                     # add raw points
+# polygon(y = c(sim$lwr_ustd, rev(sim$upr_ustd)), x = c(sim$age_cat, rev(sim$age_cat)),
+#         col = rgb(1,1,0,0.5), border = NA)
+# 
+# ## convert to invlogit scale
+# sim$mean_predict_invlogit <- invlogit(sim$predict_ustd)
+# sim$lwr_invlogit <- invlogit(sim$lwr_ustd)
+# sim$upr_invlogit <- invlogit(sim$upr_ustd)
+# sim$mu_invlogit <- invlogit(sim$mu)
+# 
+# ## compare to invlogit raw data
+# plot(sim$mu_invlogit ~ sim$age_cat, ylim = c(0,1))
+# lines(sim$mean_predict_invlogit ~ sim$age_cat)
+# polygon(y = c(sim$lwr_invlogit, rev(sim$upr_invlogit)), x = c(sim$age_cat, rev(sim$age_cat)),
+#         col = rgb(1,1,0,0.5), border = NA)
+# 
+# ## extract slope estimates -- none of these match the original input, but the predictions work??!
+# # original (true) slope value
+# sim_slope
+# # predicted (modelled) slope value = mean of differences bewteen categories, divided by the number of years each category represents
+# mean( c( (new_data$predict_ustd[2] - new_data$predict_ustd[1]) / (16-10), 
+#          (new_data$predict_ustd[3] - new_data$predict_ustd[2]) / (21-16), 
+#          (new_data$predict_ustd[4] - new_data$predict_ustd[3]) / (26-21),
+#          (new_data$predict_ustd[5] - new_data$predict_ustd[4]) / (40-26) )
+# )
+# 
+# ## extract model fit
+# summary <- fit_sim$summary()
+# par(mfrow = c(3,1))
+# hist(summary$rhat, breaks = 50)
+# hist(summary$ess_bulk, breaks = 50)
+# hist(summary$ess_tail, breaks = 50)
+# par(mfrow = c(1,1))
+# 
