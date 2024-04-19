4cbd552e96564ad1872a86709264c68cfbebab79
HelenMylne <hkm513@york.ac.uk>
2024-04-19 08:51:12 +0100

    change fixed effect to random effect and clean up

--- other/methods_paper/frequentist_mixture.R
+++ other/methods_paper/frequentist_mixture.R
@@ -21,13 +21,16 @@ rm(list = ls()[! ls() %in% c('counts_df')])
 
 #### run model ####
 ## fit a zero-inflated binomial model using glmmTMB
-mixture_fit_joint_random <- glmmTMB(cbind(event_count, apart) ~ as.factor(dyad_males),
+mixture_fit_joint_random <- glmmTMB(cbind(event_count, apart) ~ (1|dyad_males),
                                     ziformula = ~1,
                                     data = counts_df,
                                     family = binomial(link = 'logit'))
 
 ## summary of the model
-summary(mixture_fit_joint_random)
+(summary <- summary(mixture_fit_joint_random))
+invlogit(summary$coefficients$cond[1]) # estimate
+invlogit(summary$coefficients$cond[1] + summary$coefficients$cond[2]) # upper
+invlogit(summary$coefficients$cond[1] - summary$coefficients$cond[2]) # lower
 
 ## view data used for model
 head(mixture_fit_joint_random$frame)
@@ -81,14 +84,10 @@ plot(edges$prdctn_invlogit ~ edges$count_dyad)  # looks similar to other plots
     geom_bar(aes(x = round(prdctn_invlogit, 3)),
              fill = rgb(33/255, 145/255, 140/255),
              colour = rgb(33/255, 145/255, 140/255), linewidth = 0.2)+
-    scale_x_continuous(name = 'mixture model weight')+
+    scale_x_continuous(name = 'mixture model weight',
+                       breaks = c(0.00,0.10,0.20))+
     scale_y_continuous(name = 'number of dyads',
                        expand = c(0,0))+
-    # annotate('text', x = 0.5, y = 165,
-    #          label = paste0(length(which(edges$prdctn_invlogit == 0)),
-    #                         ' dyads with \nSRI = 0'),
-    #          size = unit(4, 'pt'),
-    #          colour = rgb(33/255, 145/255, 140/255))+
     coord_cartesian(ylim = c(0,5300))
 )
 
@@ -144,33 +143,139 @@ ggsave(filename = 'outputs_mixturemodel.png',
        path = '../outputs/sparse_network_methods_figures/',
        plot = last_plot(), device = 'png', width = 1600, height = 700, units = 'px')
 
-#### I think these are wrong ####
-## fit a zero-inflated Poisson model using glmmTMB
-mixture_fit_joint <- glmmTMB(cbind(event_count, apart) ~ 1,
-                             ziformula = ~1, data = counts_df, family = binomial(link = 'logit'))
-mixture_fit_together <- glmmTMB(event_count ~ count_dyad,
-                                ziformula = ~1, data = counts_df, family = poisson)
-mixture_fit_together_random <- glmmTMB(event_count ~ count_dyad,
-                                       ziformula = ~1, data = counts_df, family = poisson)
+#### calculate eigenvector ####
+# identify elephants
+elephants <- unique(c(counts_df$id_1, counts_df$id_2))
 
-## summary of the model
-summary(mixture_fit_joint)
-summary(mixture_fit_together)
+# calculate adjacency matrix
+mix_adjmat <- matrix(NA, nrow = length(elephants), ncol = length(elephants),
+                     dimnames = list(x = elephants, y = elephants))
+for(i in 1:nrow(mix_adjmat)){
+  for(j in 1:ncol(mix_adjmat)){
+    if(i <= j){
+      mix_adjmat[i,j] <- ifelse(i == j, 0,
+                                edges$prediction[which(counts_df$id_1 == rownames(mix_adjmat)[i] &
+                                                      counts_df$id_2 == colnames(mix_adjmat)[j])])
+    } else {
+      mix_adjmat[i,j] <- mix_adjmat[j,i]
+    }
+  }
+}
+mix_adjmat <- invlogit(mix_adjmat)
 
-## view data used for model
-head(mixture_fit_joint$frame)
-View(mixture_fit_together$frame) # takes data from both variables
+# create network
+mix_net <- igraph::graph_from_adjacency_matrix(mix_adjmat, weighted = T, mode = 'undirected', diag = FALSE)
 
-## extract fit
-mixture_fit_joint$fit
-mixture_fit_together$fit
+# calculate eigenvector centrality
+eigen_mix <- igraph::eigen_centrality(mix_net)$vector %>%
+  as.data.frame()
+colnames(eigen_mix) <- 'eigen'
+eigen_mix$id <- rownames(eigen_mix)
 
-## extract standard error
-mixture_fit_joint$sdr
-mixture_fit_together$sdr
+# create data frame showing number of sightings per individual with other elephants
+together0 <- counts_df %>% filter(event_count == 0)
+eigen_mix$together0 <- NA ; for(i in 1:nrow(eigen_mix)) {
+  x <- together0 %>%
+    filter(id_1 == eigen_mix$id[i] | id_2 == eigen_mix$id[i])
+  eigen_mix$together0[i] <- nrow(x)
+}
 
-## extract edges
-predictions <- predict(object = mixture_fit_joint, newdata = counts_df)
-hist(invlogit(predictions), breaks = 50) # all the same
-predictions <- predict(object = mixture_fit_together, newdata = counts_df)
-hist(predictions)
+# calculate number of sightings per elephant
+counts <- data.frame(id = elephants, count = NA)
+for(i in 1:nrow(counts)){
+  counts$count[i] <- ifelse(i == nrow(counts),
+                            unique(counts_df$count_2[which(counts_df$id_2 == counts$id[i])]),
+                            unique(counts_df$count_1[which(counts_df$id_1 == counts$id[i])]))
+}
+eigen_mix <- eigen_mix %>%
+  left_join(counts, by = 'id')
+
+# plot
+(eigensightings_mix.1 <- ggplot(eigen_mix, aes(x = count, y = eigen))+
+    geom_point(colour = rgb(33/255, 145/255, 140/255), size = 0.5, shape = 19)+
+    scale_x_continuous(name = 'total node sightings')+
+    scale_y_continuous(name = 'node centrality', limits = c(-0.02,1.02), expand = c(0,0))
+)
+ggsave(filename = 'eigensightings_mix_noline.png',
+       path = '../outputs/sparse_network_methods_figures/',
+       plot = eigensightings_mix.1, device = 'png', width = 1600, height = 700, units = 'px')
+
+(eigensightings_mix.2 <- ggplot(eigen_mix, aes(x = count, y = eigen))+
+    geom_point(colour = rgb(33/255, 145/255, 140/255),
+               size = 0.5, shape = 19)+
+    geom_smooth(colour = rgb(68/255, 1/255, 84/255))+
+    scale_x_continuous(name = 'total node sightings')+
+    scale_y_continuous(name = 'node centrality', limits = c(-0.02,1.02), expand = c(0,0))
+)
+ggsave(filename = 'eigensightings_mix_withline.png',
+       path = '../outputs/sparse_network_methods_figures/',
+       plot = eigensightings_mix.2, device = 'png', width = 1600, height = 700, units = 'px')
+
+(eigen0_mix.1 <- ggplot(eigen_mix, aes(x = together0, y = eigen))+
+    geom_point(colour = rgb(33/255, 145/255, 140/255), size = 0.5, shape = 19)+
+    scale_x_continuous(name = 'dyads together = 0')+
+    scale_y_continuous(name = 'node centrality', limits = c(-0.02,1.02), expand = c(0,0))
+)
+ggsave(filename = 'eigentogether0_mix_noline.png',
+       path = '../outputs/sparse_network_methods_figures/',
+       plot = eigen0_mix.1, device = 'png', width = 1600, height = 700, units = 'px')
+
+(eigen0_mix.2 <- ggplot(eigen_mix, aes(x = together0, y = eigen))+
+    geom_point(colour = rgb(33/255, 145/255, 140/255), size = 0.5, shape = 19)+
+    geom_smooth(colour = rgb(68/255, 1/255, 84/255))+
+    scale_x_continuous(name = 'dyads together = 0')+
+    scale_y_continuous(name = 'node centrality', limits = c(-0.02,1.02), expand = c(0,0))
+)
+ggsave(filename = 'eigentogether0_mix_withline.png',
+       path = '../outputs/sparse_network_methods_figures/',
+       plot = eigen0_mix.2, device = 'png', width = 1600, height = 700, units = 'px')
+
+## change line breaks
+(eigen0_mix.2 <- ggplot(eigen_mix, aes(x = together0, y = eigen))+
+    geom_point(colour = rgb(33/255, 145/255, 140/255), size = 0.5, shape = 19)+
+    geom_smooth(colour = rgb(68/255, 1/255, 84/255))+
+    scale_x_continuous(name = 'dyads together = 0',
+                       breaks = c(100,150,200))+
+    scale_y_continuous(name = 'node centrality', limits = c(-0.02,1.02), expand = c(0,0))
+)
+
+# save workspace
+save.image('../outputs/sparse_network_methods_figures/mixture_model.RData')
+
+# merge
+(eigen0_mix.2 + eigensightings_mix.2) +
+  plot_annotation(tag_levels = 'a')
+ggsave(filename = 'eigen_outputs_mixture.png',
+       path = '../outputs/sparse_network_methods_figures/',
+       plot = last_plot(), device = 'png', width = 1600, height = 700, units = 'px')
+
+# #### I think these are wrong ####
+# ## fit a zero-inflated Poisson model using glmmTMB
+# mixture_fit_joint <- glmmTMB(cbind(event_count, apart) ~ 1,
+#                              ziformula = ~1, data = counts_df, family = binomial(link = 'logit'))
+# mixture_fit_together <- glmmTMB(event_count ~ count_dyad,
+#                                 ziformula = ~1, data = counts_df, family = poisson)
+# mixture_fit_together_random <- glmmTMB(event_count ~ count_dyad,
+#                                        ziformula = ~1, data = counts_df, family = poisson)
+# 
+# ## summary of the model
+# summary(mixture_fit_joint)
+# summary(mixture_fit_together)
+# 
+# ## view data used for model
+# head(mixture_fit_joint$frame)
+# View(mixture_fit_together$frame) # takes data from both variables
+# 
+# ## extract fit
+# mixture_fit_joint$fit
+# mixture_fit_together$fit
+# 
+# ## extract standard error
+# mixture_fit_joint$sdr
+# mixture_fit_together$sdr
+# 
+# ## extract edges
+# predictions <- predict(object = mixture_fit_joint, newdata = counts_df)
+# hist(invlogit(predictions), breaks = 50) # all the same
+# predictions <- predict(object = mixture_fit_together, newdata = counts_df)
+# hist(predictions)
